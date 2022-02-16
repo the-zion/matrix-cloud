e8b234e4296652cb5bae87de406daca954245cde
liusiyuan <945212191@qq.com>
2022-02-16 20:16:37 +0800

    feat(componnet): 增加comment模块

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,11 +2,13 @@ import {App} from 'vue'
 import CubeCommentDialog from './commentDialog/commentDialog.vue'
 import CubeExpressDialog from './expressDialog/expressDialog.vue'
 import CubeExpress from './express/express.vue'
+import CubeComment from './comment/comment.vue'
 
 export default {
     install(app: App) {
         app.component(CubeCommentDialog.name, CubeCommentDialog)
         app.component(CubeExpressDialog.name, CubeExpressDialog)
         app.component(CubeExpress.name, CubeExpress)
+        app.component(CubeComment.name, CubeComment)
     }
 }
