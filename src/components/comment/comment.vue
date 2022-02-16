a1adf1244479004283be8543eb217a2d86c96636
liusiyuan <945212191@qq.com>
2022-02-16 20:15:14 +0800

    feat(componnet): 增加comment模块

---
+++ src/components/comment/comment.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-row>
+    <el-avatar :size="35" :src="'../../src/assets/images/user1.png'"></el-avatar>
+    <cube-express></cube-express>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: "CubeComment",
+}
+</script>
+<script setup>
+import {defineProps} from "vue";
+
+
+
+</script>
+<style scoped>
+
+</style>
