18c52293d3c491fcc32c4d68453de2c7f4d46c4b
liusiyuan <945212191@qq.com>
2022-02-16 20:16:09 +0800

    feat(componnet): expressDialog模块完善

--- src/components/expressDialog/expressDialog.vue
+++ src/components/expressDialog/expressDialog.vue
@@ -1,15 +1,16 @@
 <template>
   <el-dialog v-model="props.visible" title="我要发表" @closed="dialogClosed" width="652px" destroy-on-close>
-    <cube-express v-model:mode="mode" @update-textarea="textareaChange" @update-picture="pictureChange"></cube-express>
+    <cube-express v-model:mode="mode" v-model:textArea="textArea" v-model:picturesBox="picturesBox"></cube-express>
     <template #footer>
       <span class="dialog-footer">
-        <el-button type="primary" @click="dialogFormVisible = false"
+        <el-button type="primary" @click="expressSubmit"
         >发表</el-button
         >
       </span>
     </template>
   </el-dialog>
 </template>
+
 
 <script>
 
@@ -18,17 +19,25 @@ export default {
 }
 </script>
 <script setup>
-import {defineProps} from 'vue'
-import {initData} from "./initData.js"
-import {controller} from "./controller.js"
+import {defineProps, ref} from 'vue'
 
-let props = defineProps({
+const props = defineProps({
   visible: Boolean
 })
-let emit = defineEmits(["update:visible"])
-let {mode, textArea, picturesBox} = initData()
+const emit = defineEmits(["update:visible"])
+const mode = ref('picture')
+const textArea = ref("")
+const picturesBox = ref()
 
-let {textareaChange, pictureChange, dialogClosed} = controller(emit, textArea, picturesBox)
+
+function expressSubmit() {
+  console.log(textArea.value)
+  console.log(picturesBox.value)
+}
+
+function dialogClosed() {
+  emit("update:visible", false)
+}
 </script>
 
 <style scoped>
