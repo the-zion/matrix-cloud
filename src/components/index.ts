c18f50e45f47fc98c17d04e3808b63a0d5f57776
liusiyuan <945212191@qq.com>
2022-02-17 23:45:09 +0800

    feat(component): emoji组件开发

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,8 @@ import CubeCommentDialog from './commentDialog/commentDialog.vue'
 import CubeExpressDialog from './expressDialog/expressDialog.vue'
 import CubeExpress from './express/express.vue'
 import CubeComment from './comment/comment.vue'
+import CubeEmoji from './emoji/emoji.vue'
+import CubeShare from './share/share.vue'
 
 export default {
     install(app: App) {
@@ -10,5 +12,7 @@ export default {
         app.component(CubeExpressDialog.name, CubeExpressDialog)
         app.component(CubeExpress.name, CubeExpress)
         app.component(CubeComment.name, CubeComment)
+        app.component(CubeEmoji.name, CubeEmoji)
+        app.component(CubeShare.name, CubeShare)
     }
 }
