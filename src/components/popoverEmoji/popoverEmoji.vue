61ab604f82ce8df1c3dd6dd1173a7ab2a87b5940
liusiyuan <945212191@qq.com>
2022-02-18 21:58:51 +0800

    feat(component): 组件 popoverEmoji 新增

---
+++ src/components/popoverEmoji/popoverEmoji.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-popover :placement="props.placement || 'right'" :width="'fit-content'" trigger="hover" popper-class="express-popover">
+    <template #reference>
+      <el-icon>
+        <img src="../../assets/images/smile.svg" style="height: 1.5em;width: 1.5em;cursor: pointer">
+      </el-icon>
+    </template>
+    <cube-emoji @emoji-click="emojiClick"></cube-emoji>
+  </el-popover>
+</template>
+
+<script>
+export default {
+  name: "CubePopoverEmoji"
+}
+</script>
+<script setup>
+import {defineProps} from "vue";
+
+const emit = defineEmits(["emoji-click"])
+const props = defineProps({
+  placement: String,
+})
+
+function emojiClick(emoji) {
+  emit("emoji-click", emoji)
+}
+</script>
+
+<style lang="scss">
+.el-popover.express-popover {
+  padding: 0 !important;
+}
+</style>
