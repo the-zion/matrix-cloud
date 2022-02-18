e0a23ee733e1d43ec078727aaaa3155789327b1a
liusiyuan <945212191@qq.com>
2022-02-18 21:59:54 +0800

    feat(component): 新组件引入

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,7 +4,10 @@ import CubeExpressDialog from './expressDialog/expressDialog.vue'
 import CubeExpress from './express/express.vue'
 import CubeComment from './comment/comment.vue'
 import CubeEmoji from './emoji/emoji.vue'
+import CubePopoverEmoji from './popoverEmoji/popoverEmoji.vue'
 import CubeShare from './share/share.vue'
+import CubeReply from './reply/reply.vue'
+import CubeInput from './input/input.vue'
 
 export default {
     install(app: App) {
@@ -13,6 +16,9 @@ export default {
         app.component(CubeExpress.name, CubeExpress)
         app.component(CubeComment.name, CubeComment)
         app.component(CubeEmoji.name, CubeEmoji)
+        app.component(CubePopoverEmoji.name, CubePopoverEmoji)
         app.component(CubeShare.name, CubeShare)
+        app.component(CubeReply.name, CubeReply)
+        app.component(CubeInput.name, CubeInput)
     }
 }
