02708e17372a2b6abef69a54237973b926a2c070
liusiyuan <945212191@qq.com>
2022-02-21 23:38:35 +0800

    feat(page): user 开发

---
+++ src/pages/user/user.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-container class="user-container">
+    <el-aside class="user-aside">
+      <cube-user-info></cube-user-info>
+    </el-aside>
+  </el-container>
+</template>
+
+<script setup>
+import {ref} from "vue"
+const test = ref(true)
+</script>
+
+<style scoped lang="scss">
+.user-container {
+  height: 100%;
+
+  .user-aside {
+    padding: 24px;
+    border-right: 1px solid var(--el-border-color-base);
+  }
+}
+</style>
