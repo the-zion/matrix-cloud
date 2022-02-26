f83c8f764a5e989eb031f7a273c65bc8674d0791
liusiyuan <945212191@qq.com>
2022-02-26 23:47:02 +0800

    feat(page): about开发ing

--- src/pages/about/about.vue
+++ src/pages/about/about.vue
@@ -1,14 +1,5 @@
 <template>
-  <el-descriptions title="User Info">
-    <el-descriptions-item label="Username">kooriookami</el-descriptions-item>
-    <el-descriptions-item label="Telephone">18100000000</el-descriptions-item>
-    <el-descriptions-item label="Place">Suzhou</el-descriptions-item>
-    <el-descriptions-item >
-      <el-tag size="small">School</el-tag>
-    </el-descriptions-item>
-    <el-descriptions-item label="Address"
-    >No.1188, Wuzhong Avenue, Wuzhong District, Suzhou, Jiangsu
-      Province</el-descriptions-item
-    >
-  </el-descriptions>
+  <cube-blog-introduce></cube-blog-introduce>
 </template>
+<script>
+</script>
