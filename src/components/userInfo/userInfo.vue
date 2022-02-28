0f4921907cbe53eb1c8ba8d3a3356e7783484122
liusiyuan <945212191@qq.com>
2022-02-28 23:55:59 +0800

    feat(component): 组件 userinfo 开发

--- src/components/userInfo/userInfo.vue
+++ src/components/userInfo/userInfo.vue
@@ -2,6 +2,12 @@
   <el-row>
     <el-space fill class="body" size="large">
       <el-row justify="center" class="base-info">
+        <el-row class="setting" justify="end">
+          <cube-dialog :visible="visible" :define="dialogDefine"></cube-dialog>
+          <el-icon @click="visible = !visible">
+            <setting/>
+          </el-icon>
+        </el-row>
         <el-space direction="vertical" :size="25">
           <el-avatar :size="104" src="../src/assets/images/user.jpg"></el-avatar>
           <el-space fill :size="4">
@@ -22,7 +28,9 @@
               <el-row class="info-word">945212191@qq.com</el-row>
             </el-space>
             <el-space>
-              <el-icon><location /></el-icon>
+              <el-icon>
+                <location/>
+              </el-icon>
               <el-row class="info-word">浙江省杭州市</el-row>
             </el-space>
           </el-space>
@@ -50,10 +58,63 @@
 
 <script>
 export default {
-  name: "CubeUserInfo"
+  name: "CubeUserInfo",
 }
 </script>
 <script setup>
+import {ref} from "vue"
+import {ElMessage} from "element-plus";
+
+const visible = ref(false)
+const dialogDefine = ref({
+  title: "设置",
+  top: "5vh",
+  mode: "form",
+  footBtn: [{
+    name: "取消",
+    click: function () {
+      visible.value = !visible.value
+    },
+  }, {
+    name: "确认",
+    type: "primary",
+    click: function () {
+      visible.value = !visible.value
+    },
+  }],
+  beforeClose: function () {
+    visible.value = !visible.value
+  },
+  formDefine: {
+    form: [{
+      key: "image",
+      labelWidth: '0px',
+      align: "middle",
+      justify: "center",
+      style: "width:100%",
+      size: 150,
+      component: "el-upload",
+      onChange: function (fileObject) {
+        let file = fileObject.raw
+        const isJPG = (file.type === 'image/jpeg') || (file.type === 'image/png') || (file.type === 'image/gif')
+        const isLt2M = file.size / 1024 / 1024 < 2
+
+        if (!isJPG) {
+          ElMessage.error('只能上传 JPG/PNG/GIF 格式的图片!')
+        }
+        if (!isLt2M) {
+          ElMessage.error('图片大小不得大于 2MB!')
+        }
+        dialogDefine.value.formDefine.data.image = URL.createObjectURL(file)
+        console.log(URL.createObjectURL(file))
+        return isJPG && isLt2M
+      }
+
+    }],
+    data: {image: "../src/assets/images/user.jpg"}
+  }
+})
+
 </script>
 
 <style scoped lang="scss">
@@ -62,6 +123,13 @@ export default {
 
   .base-info {
     border-bottom: 1px solid var(--el-border-color-base);
+
+    .setting {
+      width: 100%;
+      font-size: 20px;
+      color: #909399;
+      cursor: pointer;
+    }
 
     .name {
       font-size: 20px;
