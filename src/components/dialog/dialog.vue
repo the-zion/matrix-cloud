8dec0b59dba0b40cf710ff6493c9a841fb5f3d3f
liusiyuan <945212191@qq.com>
2022-02-28 23:54:49 +0800

    feat(component): 组件 dialog 开发

---
+++ src/components/dialog/dialog.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-dialog
+      destroy-on-close
+      v-model="props.visible"
+      :title="props.define.title"
+      :width="props.define.width"
+      :top="props.define.top"
+      :before-close="props.define.beforeClose"
+  >
+    <cube-form v-if="props.define.mode === 'form'" :define="props.define.formDefine"></cube-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button :type="btn.type" @click="btn.click" v-for="btn in props.define.footBtn">{{ btn.name }}</el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  name: "CubeDialog",
+}
+</script>
+<script setup>
+import {defineProps} from "vue";
+
+const props = defineProps({
+  visible: Boolean,
+  define: Object
+})
+</script>
+
+<style scoped>
+
+</style>
