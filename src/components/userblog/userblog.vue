04e49677453ea5aab1ff8d6576e9f411003fe114
liusiyuan <945212191@qq.com>
2022-03-01 23:51:19 +0800

    feat(component): 组件userblog开发ing

--- src/components/userblog/userblog.vue
+++ src/components/userblog/userblog.vue
@@ -1,22 +1,20 @@
 <template>
   <el-container>
     <el-main>
-      <el-timeline>
-        <el-timeline-item
-            v-for="(activity, index) in activities"
-            :key="index"
-            :icon="activity.icon"
-            :type="activity.type"
-            :color="activity.color"
-            :size="activity.size"
-            :hollow="activity.hollow"
-            :timestamp="activity.timestamp"
-        >
-          {{ activity.content }}
-        </el-timeline-item>
-      </el-timeline>
+      <cube-blog-introduce v-for="item in datalist" :row="item"
+                           :style="'padding:16px 0px;margin:0 40px;border-bottom:1px solid var(--el-border-color-base)'"></cube-blog-introduce>
     </el-main>
-    <el-footer></el-footer>
+    <el-footer>
+      <el-pagination
+          v-model:currentPage="currentPage"
+          class="home-pagination"
+          :page-size="100"
+          layout="prev, pager, next, jumper"
+          :total="1000"
+          @current-change="pageCurrentChange"
+      >
+      </el-pagination>
+    </el-footer>
   </el-container>
 </template>
 
@@ -27,37 +25,33 @@ export default {
 </script>
 
 <script setup>
-import {MoreFilled} from '@element-plus/icons-vue'
+import {ref} from "vue"
+import {globalFunc} from "../../utils/globalFunc";
 
-const activities = [
-  {
-    content: 'Custom icon',
-    timestamp: '2018-04-12 20:46',
-    size: 'large',
-    type: 'primary',
-    icon: MoreFilled,
-  },
-  {
-    content: 'Custom color',
-    timestamp: '2018-04-03 20:46',
-    color: '#0bbd87',
-  },
-  {
-    content: 'Custom size',
-    timestamp: '2018-04-03 20:46',
-    size: 'large',
-  },
-  {
-    content: 'Custom hollow',
-    timestamp: '2018-04-03 20:46',
-    type: 'primary',
-    hollow: true,
-  },
-  {
-    content: 'Default node',
-    timestamp: '2018-04-03 20:46',
-  },
-]
+const datalist = ref([])
+const currentPage  =ref(1)
+const backTop = ref()
+const {loadFullScreen} = globalFunc()
+
+function pageCurrentChange(item) {
+  console.log(item)
+  let loading = loadFullScreen()
+  setTimeout(() => {
+    loading.close()
+  }, 1000)
+}
+
+for (let i = 0; i < 20; i++) {
+  datalist.value.push({
+    id: '1',
+    date: '2016-05-03',
+    name: 'TomTomTomTomTomTomTom',
+    tag: "数据结构和算法",
+    introduction: 'Docker从入门到入土Docker从入门到入土Docker从入门到入土Docker从入门到入土',
+    content: '容器其实是一种沙盒技术。顾名思义，沙盒就是能够像一个集装箱一样，把你的应用“装”起来的技术。这样，应用与应用之间，就因为有了边界而不至于相互干扰。',
+    title: "Docker入门"
+  })
+}
 </script>
 
 <style scoped>
