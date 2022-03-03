d240eb32b79e995c8a6076aef7a8e9af3cf2651d
liusiyuan <945212191@qq.com>
2022-03-03 23:13:38 +0800

    feat(component): 组件aside开发

---
+++ src/components/aside/aside.vue
@@ -0,0 +1,55 @@
+<template>
+  <el-aside class="aside" width="208px">
+    <el-menu
+        default-active="0"
+        class="aside-menu"
+        @select="menuSelect"
+    >
+      <el-menu-item v-for="(item,index) in props.classList" :index="index+''" class="aside-menu-item"
+                    @click="classClick">
+        <el-icon>
+          <component :is="item.icon"></component>
+        </el-icon>
+        <span>{{ item.label }}</span>
+      </el-menu-item>
+    </el-menu>
+  </el-aside>
+</template>
+
+<script>
+export default {
+  name: "CubeAside"
+}
+</script>
+<script setup>
+import {defineProps, defineEmits} from "vue";
+
+const emits = defineEmits(["select", "classClick"])
+const props = defineProps({
+  classList: {
+    type: Object,
+    default: {}
+  },
+})
+
+function menuSelect(index, indexPath, item, routeResult) {
+  emits("select", index, indexPath, item, routeResult)
+}
+
+function classClick(item) {
+  emits("classClick", item)
+}
+</script>
+
+<style scoped lang="scss">
+.aside {
+  .aside-menu {
+    height: 100%;
+    border-right: unset;
+
+    .el-menu-item.is-active {
+      background: var(--el-menu-hover-bg-color);
+    }
+  }
+}
+</style>
