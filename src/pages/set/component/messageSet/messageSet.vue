3583f2ff7f61cb37d58a44633cda7d11e10b50c8
liusiyuan <945212191@qq.com>
2022-03-05 11:56:48 +0800

    feat(pages): set模块开发

--- src/pages/set/component/messageSet/messageSet.vue
+++ src/pages/set/component/messageSet/messageSet.vue
@@ -4,8 +4,8 @@
     <el-space fill class="block">
       <el-row justify="space-between">
         <el-col :span="4" class="word">用户消息</el-col>
-        <el-col :span="2" @click="passWordModify">
-          <el-switch v-model="userMessageSwitch"/>
+        <el-col :span="2">
+          <el-switch v-model="userSwitch" @change="userSwitchChange"/>
         </el-col>
       </el-row>
       <el-row class="word color">其他用户的消息将以站内信的形式通知</el-row>
@@ -14,7 +14,7 @@
       <el-row justify="space-between">
         <el-col :span="4" class="word">系统消息</el-col>
         <el-col :span="2" @click="phoneModify">
-          <el-switch v-model="systemMessageSwitch"/>
+          <el-switch v-model="systemSwitch" @change="systemSwitchChange"/>
         </el-col>
       </el-row>
       <el-row class="word color">系统消息将以站内信的形式通知</el-row>
@@ -30,8 +30,16 @@ export default {
 <script setup>
 import {ref} from "vue"
 
-const userMessageSwitch = ref(true)
-const systemMessageSwitch = ref(true)
+const userSwitch = ref(true)
+const systemSwitch = ref(true)
+
+function userSwitchChange(item) {
+  console.log(item)
+}
+
+function systemSwitchChange(item) {
+  console.log(item)
+}
 </script>
 
 <style scoped lang="scss">
