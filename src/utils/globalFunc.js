69d72efc2505b862ad57f2c99131b2564f2417f7
liusiyuan <945212191@qq.com>
2022-03-05 11:54:43 +0800

    feat(utils): 全局函数getClassList引入

--- src/utils/globalFunc.js
+++ src/utils/globalFunc.js
@@ -1,4 +1,3 @@
-
 import {ElLoading} from 'element-plus'
 
 export function globalFunc() {
@@ -10,5 +9,84 @@ export function globalFunc() {
         })
     }
 
-    return {loadFullScreen}
+    function getClassList() {
+        return [{
+            "value": "language",
+            "label": "编程语言",
+            "children": [{
+                "value": "python",
+                "label": "Python",
+            }, {
+                "value": "go",
+                "label": "Go",
+            }, {
+                "value": "java",
+                "label": "Java",
+            }, {
+                "value": "javaScript",
+                "label": "JavaScript",
+            }, {
+                "value": "c++",
+                "label": "C++",
+            }, {
+                "value": "c",
+                "label": "C",
+            }],
+        }, {
+            "value": "middleware",
+            "label": "中间件",
+            "children": [{
+                "value": "redis",
+                "label": "Redis",
+            }, {
+                "value": "rabbitmq",
+                "label": "Rabbitmq",
+            }]
+        }, {
+            "value": "virtualization",
+            "label": "云原生",
+            "children": [{
+                "value": "docker",
+                "label": "Docker",
+            }, {
+                "value": "kubernetes",
+                "label": "kubernetes",
+            }, {
+                "value": "microServices",
+                "label": "微服务",
+            }]
+        }, {
+            "value": "database",
+            "label": "数据库",
+            "children": [{
+                "value": "mysql",
+                "label": "Mysql",
+            }]
+        }, {
+            "value": "basics",
+            "label": "计算机基础",
+            "children": [{
+                "value": "network",
+                "label": "网络",
+            }, {
+                "value": "dataStructure",
+                "label": "数据结构和算法",
+            }, {
+                "value": "operatingSystem",
+                "label": "操作系统",
+            }, {
+                "value": "computerComposition",
+                "label": "计算机组成原理",
+            }]
+        }, {
+            "value": "other",
+            "label": "其他",
+            "children": [{
+                "value": "live",
+                "label": "生活",
+            }]
+        }]
+    }
+
+    return {loadFullScreen, getClassList}
 }
