8ff47edfde5a2617ca44bc4bffe52773ecfd6b59
liusiyuan <945212191@qq.com>
2022-05-02 23:56:12 +0800

    refactor(utils): 删除login路由

--- src/router/index.js
+++ src/router/index.js
@@ -10,11 +10,11 @@ import Blog from '../pages/blog/blog.vue'
 import Message from '../pages/message/message.vue'
 import Search from '../pages/search/search.vue'
 import About from '../pages/about/about.vue'
-import Login from "../pages/login/login.vue"
-import LoginMain from "../pages/login/main/main.vue"
-import Code from "../pages/login/code/code.vue"
-import Password from "../pages/login/password/password.vue"
-import PasswordReset from "../pages/login/passwordReset/passwordReset.vue"
+// import Login from "../pages/login/login.vue"
+// import LoginMain from "../pages/login/main/main.vue"
+// import Code from "../pages/login/code/code.vue"
+// import Password from "../pages/login/password/password.vue"
+// import PasswordReset from "../pages/login/passwordReset/passwordReset.vue"
 import User from "../pages/user/user.vue"
 import Set from "../pages/set/set.vue"
 
@@ -82,34 +82,34 @@ const routes = [
             }]
 
     },
-    {
-        path: '/login',
-        name: 'login',
-        component: Login,
-        redirect: {name: 'loginMain'},
-        children: [
-            {
-                path: 'loginMain',
-                name: 'loginMain',
-                component: LoginMain,
-            },
-            {
-                path: 'code',
-                name: 'code',
-                component: Code,
-            },
-            {
-                path: 'password',
-                name: 'password',
-                component: Password
-            },
-            {
-                path: 'passwordReset',
-                name: 'passwordReset',
-                component: PasswordReset
-            }
-        ]
-    }
+    // {
+    //     path: '/login',
+    //     name: 'login',
+    //     component: Login,
+    //     redirect: {name: 'loginMain'},
+    //     children: [
+    //         {
+    //             path: 'loginMain',
+    //             name: 'loginMain',
+    //             component: LoginMain,
+    //         },
+    //         {
+    //             path: 'code',
+    //             name: 'code',
+    //             component: Code,
+    //         },
+    //         {
+    //             path: 'password',
+    //             name: 'password',
+    //             component: Password
+    //         },
+    //         {
+    //             path: 'passwordReset',
+    //             name: 'passwordReset',
+    //             component: PasswordReset
+    //         }
+    //     ]
+    // }
 ]
 
 const router = createRouter({
