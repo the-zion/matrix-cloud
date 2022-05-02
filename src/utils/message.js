59ea85346312ed90cacbe0e46109733bf5468c9f
liusiyuan <945212191@qq.com>
2022-05-02 23:54:39 +0800

    feat(utils): 添加全局message

---
+++ src/utils/message.js
@@ -0,0 +1,21 @@
+import {ElMessage} from "element-plus";
+
+export function message() {
+    function success(message) {
+        ElMessage({
+            message: message,
+            type: 'success',
+            grouping: true
+        })
+    }
+
+    function error(message){
+        ElMessage({
+            message: message,
+            type: 'error',
+            grouping: true
+        })
+    }
+
+    return {success, error}
+}
