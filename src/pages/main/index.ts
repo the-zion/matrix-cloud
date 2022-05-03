106f6693a1ea7b9f26762c9946543f44014e7349
liusiyuan <945212191@qq.com>
2022-05-03 23:33:04 +0800

    refactor(pages/main): 新增注册与密码修改组件

--- src/pages/main/index.ts
+++ src/pages/main/index.ts
@@ -3,11 +3,15 @@ import {App} from 'vue'
 import CubeLogin from './component/login/main.vue'
 import CubeLoginAccount from './component/login/account/account.vue'
 import CubeLoginCode from './component/login/code/code.vue'
+import CubeLoginRegister from './component/login/register/register.vue'
+import CubeLoginForget from './component/login/forget/forget.vue'
 
 export default {
     install(app: App) {
         app.component(CubeLogin.name, CubeLogin)
         app.component(CubeLoginAccount.name, CubeLoginAccount)
         app.component(CubeLoginCode.name, CubeLoginCode)
+        app.component(CubeLoginRegister.name, CubeLoginRegister)
+        app.component(CubeLoginForget.name, CubeLoginForget)
     }
 }
