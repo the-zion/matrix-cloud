369343a39aae57e413d087efdb63a4c1d8d1efed
liusiyuan <945212191@qq.com>
2022-05-04 23:32:26 +0800

    refactor(user): user模块主界面逻辑修改

--- src/pages/user/user.vue
+++ src/pages/user/user.vue
@@ -1,35 +1,38 @@
 <template>
-  <el-container class="user-container" direction="vertical">
+  <el-container class="container" direction="vertical">
     <el-backtop target=".el-scrollbar__wrap" ref="backTop"></el-backtop>
     <!--    <el-aside class="user-aside">-->
     <!--      <cube-user-info></cube-user-info>-->
     <!--    </el-aside>-->
-    <el-row class="user-info">
-      <cube-user-info></cube-user-info>
-    </el-row>
-    <el-main class="user-main">
-      <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
-        <el-tab-pane v-for="item in menu" :name="item.name">
-          <template #label>
-            <el-space>
-              <el-icon>
-                <component :is="item.icon"></component>
-              </el-icon>
-              <el-row>{{ item.label }}</el-row>
-            </el-space>
-          </template>
-        </el-tab-pane>
-      </el-tabs>
-      <el-row :style="activeName !== 'time' && {'height':'calc(100% - 45px)'}">
-        <cube-time-line v-if="activeName === 'time'"></cube-time-line>
-        <cube-user-blog v-if="activeName === 'blog'" @upToTop="upToTop"></cube-user-blog>
-        <cube-user-talk v-if="activeName === 'talk'" @upToTop="upToTop"></cube-user-talk>
-        <cube-user-column v-if="activeName === 'column'" @upToTop="upToTop"></cube-user-column>
-        <cube-user-collect v-if="activeName === 'collect'" @upToTop="upToTop"></cube-user-collect>
-        <cube-user-care v-if="activeName === 'care' || activeName === 'cared'" @upToTop="upToTop"></cube-user-care>
-        <cube-user-message v-if="activeName === 'message'" @upToTop="upToTop"></cube-user-message>
-      </el-row>
-    </el-main>
+    <el-header class="header">
+      <cube-user-card></cube-user-card>
+    </el-header>
+    <el-aside class="aside">
+      <cube-user-profile></cube-user-profile>
+    </el-aside>
+    <!--    <el-main class="user-main">-->
+    <!--      <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">-->
+    <!--        <el-tab-pane v-for="item in menu" :name="item.name">-->
+    <!--          <template #label>-->
+    <!--            <el-space>-->
+    <!--              <el-icon>-->
+    <!--                <component :is="item.icon"></component>-->
+    <!--              </el-icon>-->
+    <!--              <el-row>{{ item.label }}</el-row>-->
+    <!--            </el-space>-->
+    <!--          </template>-->
+    <!--        </el-tab-pane>-->
+    <!--      </el-tabs>-->
+    <!--      <el-row :style="activeName !== 'time' && {'height':'calc(100% - 45px)'}">-->
+    <!--        <cube-time-line v-if="activeName === 'time'"></cube-time-line>-->
+    <!--        <cube-user-blog v-if="activeName === 'blog'" @upToTop="upToTop"></cube-user-blog>-->
+    <!--        <cube-user-talk v-if="activeName === 'talk'" @upToTop="upToTop"></cube-user-talk>-->
+    <!--        <cube-user-column v-if="activeName === 'column'" @upToTop="upToTop"></cube-user-column>-->
+    <!--        <cube-user-collect v-if="activeName === 'collect'" @upToTop="upToTop"></cube-user-collect>-->
+    <!--        <cube-user-care v-if="activeName === 'care' || activeName === 'cared'" @upToTop="upToTop"></cube-user-care>-->
+    <!--        <cube-user-message v-if="activeName === 'message'" @upToTop="upToTop"></cube-user-message>-->
+    <!--      </el-row>-->
+    <!--    </el-main>-->
   </el-container>
 </template>
 
@@ -46,18 +49,18 @@ const {upToTop} = controller(backTop)
 </script>
 
 <style scoped lang="scss">
-.user-container {
-  //height: 100%;
-  width: 1000px;
+.container {
+  width: 1200px;
   margin: auto;
 
-  .user-info {
-    height: 275px;
-    width: 100%;
-    background: var(--el-color-white);
-    margin: 10px 0;
-    border-radius: 2px;
-    box-shadow: 0 1px 3px rgb(18 18 18 / 10%)
+  .header {
+    padding: unset;
+    height: fit-content;
+  }
+
+  .aside {
+    box-shadow: var(--el-box-shadow);
+    border-radius: 0.5rem;
   }
 
   .user-main {
