512f765cc66ad4ecdd82e2b152f2280e686fd7dc
liusiyuan <945212191@qq.com>
2022-05-06 00:01:41 +0800

    refactor(pages/user): user模块，initData增加配置

--- src/pages/user/initData.js
+++ src/pages/user/initData.js
@@ -36,8 +36,73 @@ export function initData() {
         }]
     )
 
+    let cartoonOptions = {
+        graphic: {
+            elements: [
+                {
+                    type: 'text',
+                    left: 'center',
+                    top: 'center',
+                    style: {
+                        text: 'HELLO WORLD',
+                        fontSize: 60,
+                        fontWeight: 'bold',
+                        lineDash: [0, 200],
+                        lineDashOffset: 0,
+                        fill: 'transparent',
+                        stroke: '#000',
+                        lineWidth: 1
+                    },
+                    keyframeAnimation: {
+                        duration: 3000,
+                        loop: true,
+                        keyframes: [
+                            {
+                                percent: 0.7,
+                                style: {
+                                    fill: 'transparent',
+                                    lineDashOffset: 200,
+                                    lineDash: [200, 0]
+                                }
+                            },
+                            {
+                                // Stop for a while.
+                                percent: 0.8,
+                                style: {
+                                    fill: 'transparent'
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    }
+
+    let calendarOptions = {
+        tooltip: {
+            show: true,
+            formatter: function (params, ticket, callback) {
+                return params.value[0] + ' ' + params.value[1] + '篇'
+            }
+        },
+        gradientColor: [
+            'rgba(255, 127, 0, 0.2)',
+            'rgba(255, 127, 0, 0.3)',
+            'rgba(255, 127, 0, 0.5)',
+            'rgba(255, 127, 0, 0.7)',
+            'rgba(255, 127, 0, 0.8)'
+        ],
+        visualMap: {
+            show: false,
+        },
+        calendar: [],
+        series: []
+    }
+
+
     return {
-        menu
+        menu, cartoonOptions, calendarOptions
     }
 
 }
