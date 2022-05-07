5568e37e8531c90b63715b93d69e98207e4d1801
liusiyuan <945212191@qq.com>
2022-05-07 17:53:29 +0800

    refactor(component.list): blog组件代码重构

--- src/components/blog/blog.vue
+++ src/components/blog/blog.vue
@@ -2,34 +2,34 @@
   <el-row class="blog">
     <el-space class="main" fill>
       <el-space class="head">
-        <el-avatar class="avatar" :size="24" :src="props.data.avatar"/>
-        <el-row class="title">{{ props.data.title }}</el-row>
+        <el-avatar class="avatar" :size="24" :src="avatar"/>
+        <el-row class="title">{{ title }}</el-row>
       </el-space>
       <el-space class="info">
-        <el-row class="name">{{ props.data.name }}</el-row>
-        <el-row class="time">{{ "发布于 " + props.data.time }}</el-row>
+        <el-row class="name">{{ name }}</el-row>
+        <el-row class="time">{{ "发布于 " + time }}</el-row>
       </el-space>
       <el-space class="body">
-        <el-image class="image" fit="cover" :src="props.data.image"></el-image>
-        <span class="content">{{ props.data.content }}</span>
+        <el-image class="image" fit="cover" :src="image"></el-image>
+        <span class="content">{{ content }}</span>
       </el-space>
     </el-space>
     <el-space class="foot">
       <el-space :size="3">
         <el-icon class="iconfont icon-like icon"></el-icon>
-        <span class="num">{{props.data.agree}}</span>
+        <span class="num">{{agree}}</span>
       </el-space>
       <el-space :size="3">
         <el-icon class="iconfont icon-eye icon"></el-icon>
-        <span class="num">{{props.data.view}}</span>
+        <span class="num">{{view}}</span>
       </el-space>
       <el-space :size="3">
         <el-icon class="iconfont icon-message icon"></el-icon>
-        <span class="num">{{props.data.comment}}</span>
+        <span class="num">{{comment}}</span>
       </el-space>
       <el-space :size="3">
         <el-icon class="iconfont icon-star icon"></el-icon>
-        <span class="num">{{props.data.collect}}</span>
+        <span class="num">{{collect}}</span>
       </el-space>
     </el-space>
   </el-row>
@@ -43,10 +43,15 @@ export default {
 
 <script setup>
 import {defineProps} from "vue"
+import {controller} from "./controller";
 
 const props = defineProps({
   data: Object,
 })
+let {initVariable,init} = controller()
+let {avatar, title, name, time, image, content, agree, view, comment, collect} = initVariable()
+init(props)
+
 </script>
 
 <style scoped lang="scss">
--- src/components/blog/variable.js
+++ src/components/blog/variable.js
@@ -1,9 +1,15 @@
-import {ref} from 'vue'
-
+import {ref} from "vue"
 export function variable() {
+    let avatar = ref()
+    let title = ref()
+    let name = ref()
+    let time = ref()
+    let image = ref()
+    let content = ref()
+    let agree = ref()
+    let view = ref()
+    let comment = ref()
+    let collect = ref()
 
-    let avatar = null
-    let data = ref([])
-
-    return {avatar, data}
+    return {avatar, title, name, time, image, content, agree, view, comment, collect}
 }
