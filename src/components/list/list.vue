08b606cfd950898d8c90c9155fb9ec13a7eb2422
liusiyuan <945212191@qq.com>
2022-05-10 14:17:44 +0800

    feat(component.list): list组件增加card样式逻辑

--- src/components/list/list.vue
+++ src/components/list/list.vue
@@ -1,8 +1,8 @@
 <template>
   <el-row class="list">
     <el-empty v-show="data.length === 0" class="empty" description="未找到相关内容"/>
-    <el-space class="data" fill>
-      <el-row v-for="item in data" class="each">
+    <el-space class="data" fill :size="props.gap || 0">
+      <el-row v-for="item in data" class="each" :class="props.shape">
         <matrix-blog v-if="props.mode === 1" :data="item"></matrix-blog>
         <matrix-column v-if="props.mode === 2" :data="item"></matrix-column>
         <matrix-talk v-if="props.mode === 3" :data="item"></matrix-talk>
@@ -33,6 +33,8 @@ import {controller} from "./controller";
 const emits = defineEmits(["current-page"])
 const props = defineProps({
   mode: Number,
+  gap: Number,
+  shape: String
 })
 const {initVariable, init} = controller()
 let {data, currentPage} = initVariable()
@@ -52,9 +54,12 @@ onMounted(() => {
 
   .data {
     width: 100%;
-    margin-top: 1rem;
 
     .each {
+      border-bottom: 1px solid var(--el-border-color);
+    }
+
+    .card {
       border-radius: 8px;
       border: 1px solid var(--el-border-color);
     }
