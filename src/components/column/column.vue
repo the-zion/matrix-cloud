142f655c9c700ef630b978db4f80c375da7e3de8
liusiyuan <945212191@qq.com>
2022-05-10 15:24:33 +0800

    refactor(component.column): column组件代码重构

--- src/components/column/column.vue
+++ src/components/column/column.vue
@@ -41,12 +41,11 @@ export default {
 </script>
 
 <script setup>
-import {controller} from "./controller";
+import {initVariable, init} from "./controller";
 
 const props = defineProps({
   data: Object,
 })
-const {initVariable, init} = controller()
 let {avatar, title, name, time, tags, image, content, agree, view, comment, collect} = initVariable()
 init(props)
 
