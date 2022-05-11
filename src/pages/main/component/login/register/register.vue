252f8517fe27153be46c03d9a9a362b110c11cdf
liusiyuan <945212191@qq.com>
2022-05-11 09:51:12 +0800

    refactor(pages.main): login模块子组件register重构

--- src/pages/main/component/login/register/register.vue
+++ src/pages/main/component/login/register/register.vue
@@ -26,15 +26,68 @@ export default {
 </script>
 
 <script setup>
-import {onMounted} from "vue"
-import {initVariable, init, register, mode} from "./controller";
+import {ref} from "vue";
+import {message} from "../../../../../utils/message";
 
 const emits = defineEmits(["update:mode", "close"])
-let {form, formRef, loading, rules} = initVariable()
 
-onMounted(function () {
-  init(emits)
+let form = ref({
+  email: "",
+  password: ""
 })
+let formRef = ref()
+let loading = ref(false)
+const rules = ref({
+  email: [{validator: validateEmail, trigger: 'blur'}],
+  password: [{validator: validatePassword, trigger: 'blur'}]
+})
+const {success, error} = message()
+
+function validateEmail(rule, value, callback) {
+  if (!checkEmail(value)) {
+    value || callback(new Error("邮箱不能为空"))
+    callback(new Error("邮箱格式错误"))
+  }
+  callback()
+}
+
+function validatePassword(rule, value, callback) {
+  value || callback(new Error("密码不能为空"))
+  callback()
+}
+
+function checkEmail(value) {
+  return value.match("^[a-z0-9]+([._\\\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$")
+}
+
+function register(formRef) {
+  if (!formRef) {
+    error("未知错误")
+    return
+  }
+  formRef.validate((valid) => {
+    if (!valid) {
+      error("邮箱输入有误，请检查")
+    } else {
+      loading.value = true
+      setTimeout(function () {
+        loading.value = false
+        success("账号注册成功")
+        closeDialog()
+      }, 500)
+      return true
+    }
+  })
+}
+
+function mode(mode) {
+  emits("update:mode", mode)
+}
+
+function closeDialog() {
+  emits("close", "")
+}
+
 </script>
 
 <style scoped lang="scss">
