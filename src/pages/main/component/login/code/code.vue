83fd1abdbfc4949fcb0972b324ba02a2f006c4e9
liusiyuan <945212191@qq.com>
2022-05-11 09:51:38 +0800

    refactor(pages.main): login模块子组件code重构

--- src/pages/main/component/login/code/code.vue
+++ src/pages/main/component/login/code/code.vue
@@ -39,14 +39,115 @@ export default {
 </script>
 
 <script setup>
-import {onMounted} from "vue"
-import {initVariable, init, sendCode, mode, login} from "./controller";
+import {ref} from "vue";
+import {message} from "../../../../../utils/message";
 
 const emits = defineEmits(["update:mode", "close"])
-let {width, text, codeSending, interval, loading, form, formRef, rules} = initVariable()
-onMounted(function () {
-  init(emits)
+
+let width = ref(68)
+let text = ref("获取验证码")
+let codeSending = ref(false)
+let interval = ref(null)
+let loading = ref(false)
+let form = ref({
+  phone: "",
+  code: ""
 })
+let formRef = ref()
+
+const rules = ref({
+  phone: [{validator: validatePhone, trigger: 'blur'}],
+  code: [{validator: validateCode, trigger: 'blur'}],
+})
+const {success, error} = message()
+
+function sendCode() {
+  if (interval.value || !checkPhone("+86" + form.value.phone)) {
+    interval.value || error("手机号有误，请检查")
+    return null
+  }
+  codeSending.value = true
+  setTimeout(function () {
+    countDown()
+  }, 500)
+}
+
+function countDown() {
+  let countdown = 300
+  codeSending.value = false
+  width.value = 90
+  text.value = countdown
+  interval.value = setInterval(() => {
+    countdown--
+    text.value = countdown
+    if (countdown === 0) {
+      cancelInterval()
+    }
+  }, 1000)
+}
+
+function cancelInterval() {
+  if (!interval.value) {
+    return null
+  }
+  width.value = 68
+  text.value = "获取验证码"
+  clearInterval(interval.value)
+  interval.value = null
+}
+
+function mode(mode) {
+  cancelInterval()
+  emits("update:mode", mode)
+}
+
+function validatePhone(rule, value, callback) {
+  if (!checkPhone("+86" + value)) {
+    value || callback(new Error("手机号不能为空"))
+    callback(new Error("手机号格式错误"))
+  }
+  callback()
+}
+
+function validateCode(rule, value, callback) {
+  if (!checkCode(value)) {
+    value || callback(new Error("验证码不能为空"))
+    callback(new Error("验证码为6位数字"))
+  }
+  callback()
+}
+
+function checkPhone(value) {
+  return value.match("^\\+[1-9]?[0-9]{7,14}$")
+}
+
+function checkCode(value) {
+  return value.match("^[0-9]{6}$")
+}
+
+function login(formRef) {
+  if (!formRef) {
+    error("未知错误")
+    return
+  }
+  formRef.validate((valid) => {
+    if (!valid) {
+      error("手机号或验证码有误，请检查")
+    } else {
+      loading.value = true
+      setTimeout(function () {
+        loading.value = false
+        success("登录成功")
+        closeDialog()
+      }, 500)
+      return true
+    }
+  })
+}
+
+function closeDialog() {
+  emits("close", "")
+}
 
 </script>
 
