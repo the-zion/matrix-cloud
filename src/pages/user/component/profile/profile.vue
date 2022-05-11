61ebf51903cfc063281c124fd1e2a831fd08cf63
liusiyuan <945212191@qq.com>
2022-05-11 09:48:09 +0800

    refactor(pages.user): user模块子组件profile重构

--- src/pages/user/component/profile/profile.vue
+++ src/pages/user/component/profile/profile.vue
@@ -122,10 +122,48 @@ export default {
 </script>
 
 <script setup>
-import {initVariable, init, follow, followDialogClose} from "./controller";
-import {onMounted} from "vue";
+import {ref, onMounted} from "vue";
 
-let {data, achievement, skills, visible} = initVariable()
+let data = ref({})
+let achievement = ref({})
+let skills = ref([])
+let visible = ref(false)
+
+function init() {
+  initData()
+}
+
+function initData() {
+  data.value = {
+    follow: 2,
+    follower: 36,
+    profile: "vegetable chicken 我不会GO，如果看到我的提交记录是GO写的，说明那天我摸了复制了代码打卡",
+    sex: "男",
+    location: "广东省深圳市",
+    school: "中国海洋大学",
+    industry: "互联网",
+    email: "945212191@qq.com"
+  }
+
+  achievement.value = {
+    view: "18.8k",
+    agree: "361",
+    collect: "329",
+  }
+
+  skills.value = [
+    "go", "kubernetes", "docker", "云原生", "javascript", "python"
+  ]
+}
+
+function follow() {
+  visible.value = true
+}
+
+function followDialogClose() {
+  visible.value = false
+}
+
 onMounted(function () {
   init()
 })
