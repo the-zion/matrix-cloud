1f7da14b2102e2b3d21465fef8c481ea09246168
liusiyuan <945212191@qq.com>
2022-05-11 09:51:54 +0800

    refactor(pages.main): login模块子组件account重构

--- src/pages/main/component/login/account/account.vue
+++ src/pages/main/component/login/account/account.vue
@@ -26,15 +26,73 @@ export default {
 </script>
 
 <script setup>
-import {onMounted} from "vue"
-import {controller} from "./controller";
+import {ref} from "vue";
+import {message} from "../../../../../utils/message";
 
-const {initVariable, init, login, mode} = controller()
 const emits = defineEmits(["update:mode", "close"])
-let {loading, form, formRef, rules} = initVariable()
-onMounted(function () {
-  init(emits)
+
+let loading = ref(false)
+let form = ref({
+  account: "",
+  password: ""
 })
+let formRef = ref()
+
+const rules = ref({
+  account: [{validator: validateAccount, trigger: 'blur'}],
+  password: [{validator: validatePassword, trigger: 'blur'}],
+})
+const {success, error} = message()
+
+function validateAccount(rule, value, callback) {
+  if (!(checkPhone("+86" + value) || checkEmail(value))) {
+    value || callback(new Error("账号不能为空"))
+    callback(new Error("手机/邮箱 格式错误"))
+  }
+  callback()
+}
+
+function validatePassword(rule, value, callback) {
+  value || callback(new Error("密码不能为空"))
+  callback()
+}
+
+function checkPhone(value) {
+  return value.match("^\\+[1-9]?[0-9]{7,14}$")
+}
+
+function checkEmail(value) {
+  return value.match("^[a-z0-9]+([._\\\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$")
+}
+
+function login(formRef) {
+  if (!formRef) {
+    error("未知错误")
+    return
+  }
+  formRef.validate((valid) => {
+    if (!valid) {
+      error("账号或密码有误，请检查")
+    } else {
+      loading.value = true
+      setTimeout(function () {
+        success("登录成功")
+        loading.value = false
+        closeDialog()
+      }, 500)
+      return true
+    }
+  })
+}
+
+function closeDialog() {
+  emits("close", "")
+}
+
+function mode(mode) {
+  emits("update:mode", mode)
+}
+
 </script>
 
 <style scoped lang="scss">
