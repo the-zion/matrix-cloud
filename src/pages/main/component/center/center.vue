fc647da60263a50a0c18946e62ca60fbe79a0866
liusiyuan <945212191@qq.com>
2022-05-11 09:50:35 +0800

    refactor(pages.main): main模块子组件center重构

--- src/pages/main/component/center/center.vue
+++ src/pages/main/component/center/center.vue
@@ -24,10 +24,55 @@ export default {
 </script>
 
 <script setup>
-import {initVariable, init} from "./controller";
-import {onMounted} from "vue";
+import {ref, onMounted} from "vue";
+import router from "../../../../router";
 
-let {name, introduce, menu} = initVariable()
+let name = ref("")
+let introduce = ref("")
+let menu = ref([
+  {
+    label: "个人主页",
+    icon: "user",
+    func: userPage
+  },
+  {
+    label: "用户中心",
+    icon: "office-building",
+    func: userCenter
+  },
+  {
+    label: "账户设置",
+    icon: "setting",
+    func: userCenter
+  },
+  {
+    label: "退出登录",
+    icon: "circle-close",
+    func: signOut
+  }
+])
+
+function init() {
+  initData()
+}
+
+function initData() {
+  name.value = "刘小圆sama"
+  introduce.value = "海纳百川，有容纳大"
+}
+
+function userPage() {
+  router.push({name: "user"})
+}
+
+function userCenter() {
+  router.push({name: "center"})
+}
+
+function signOut() {
+
+}
+
 onMounted(function () {
   init()
 })
