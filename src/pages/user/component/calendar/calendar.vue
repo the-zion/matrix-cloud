446357dcb95be792ed7ed06041abb6e4dccd7a13
liusiyuan <945212191@qq.com>
2022-05-11 09:49:27 +0800

    refactor(pages.user): user模块子组件calendar重构

--- src/pages/user/component/calendar/calendar.vue
+++ src/pages/user/component/calendar/calendar.vue
@@ -29,10 +29,97 @@ export default {
 </script>
 
 <script setup>
-import {onMounted} from "vue"
-import {initVariable, init} from "./controller";
+import {ref, onMounted} from "vue"
+import {echartsInit} from "../../../../utils/globalFunc"
 
-let {time, nums, options} = initVariable()
+let date = new Date()
+let year = date.getFullYear()
+let time = ref(year)
+let nums = ref(0)
+let echarts = echartsInit()
+let chart = null
+const options = [year, year - 1, year - 2]
+
+let calendarOptions = {
+  tooltip: {
+    show: true,
+    formatter: function (params) {
+      return params.value[0] + ' ' + params.value[1] + '篇';
+    }
+  },
+  gradientColor: [
+    'rgba(255, 127, 0, 0.2)',
+    'rgba(255, 127, 0, 0.3)',
+    'rgba(255, 127, 0, 0.5)',
+    'rgba(255, 127, 0, 0.7)',
+    'rgba(255, 127, 0, 0.8)'
+  ],
+  visualMap: {
+    min: 1,
+    max: 5,
+    type: 'piecewise',
+    orient: 'horizontal',
+    left: 'center',
+    contentColor: '#5793f3',
+    categories: [1, 2, 3, 4, 5],
+    top: 130,
+    formatter: function (value) {
+      return value + '篇'
+    }
+  },
+  calendar: {
+    top: 20,
+    left: 0,
+    right: 0,
+    dayLabel: {
+      show: false
+    },
+    cellSize: ['auto', 15],
+    range: 2016,
+    splitLine: {
+      show: false,
+    },
+    itemStyle: {
+      borderWidth: 0.5
+    },
+    yearLabel: {show: false},
+    monthLabel: {
+      color: 'rgb(175, 180, 189)',
+      nameMap: 'ZH'
+    }
+  },
+  series: {
+    type: 'heatmap',
+    coordinateSystem: 'calendar',
+    data: [[]]
+  }
+}
+
+function init() {
+  chart = echarts.init(document.getElementById('calendar'));
+  setCalenderOptions("2022")
+}
+
+function getVirtulData(year) {
+  year = year || '2017';
+  let date = +echarts.number.parseDate(year + '-01-01');
+  let end = +echarts.number.parseDate(+year + 1 + '-01-01');
+  let dayTime = 3600 * 24 * 1000;
+  let data = [];
+  for (let time = date; time < end; time += dayTime) {
+    data.push([
+      echarts.format.formatTime('yyyy-MM-dd', time),
+      Math.floor(Math.random() * 5)
+    ]);
+  }
+  return data;
+}
+
+function setCalenderOptions(year) {
+  calendarOptions.calendar.range = year
+  calendarOptions.series.data = getVirtulData(year)
+  chart.setOption(calendarOptions)
+}
 
 onMounted(() => {
   init()
