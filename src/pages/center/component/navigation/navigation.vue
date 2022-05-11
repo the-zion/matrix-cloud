557df79b539614635a894e7b2a0c3f3ea5dae332
liusiyuan <945212191@qq.com>
2022-05-11 09:53:06 +0800

    refactor(pages.center): center模块子组件navigation重构

--- src/pages/center/component/navigation/navigation.vue
+++ src/pages/center/component/navigation/navigation.vue
@@ -34,14 +34,51 @@ export default {
 </script>
 
 <script setup>
-import {initVariable, init, menuSelect} from "./controller";
-import {onMounted} from "vue";
+import {ref} from "vue";
 
 const emits = defineEmits(["change"])
-let {nickname, name, menu} = initVariable()
-onMounted(function () {
-  init(emits)
-})
+
+let nickname = ref("刘小圆sama")
+let name = ref("neo")
+let menu = ref([{
+  key: "blog",
+  label: "我的博客",
+  icon: "document",
+  select: true,
+}, {
+  key: "column",
+  label: "我的专栏",
+  icon: "files",
+  select: false
+}, {
+  key: "talk",
+  label: "讨论发起",
+  icon: "chat-dot-round",
+  select: false
+}, {
+  key: "collect",
+  label: "我的收藏",
+  icon: "star",
+  select: false
+}, {
+  key: "profile",
+  label: "个人资料",
+  icon: "user",
+  select: false
+}, {
+  key: "security",
+  label: "账号安全",
+  icon: "lock",
+  select: false
+}])
+
+function menuSelect(each) {
+  menu.value.forEach(function (item) {
+    item.select = each.key === item.key
+  })
+  emits("change", each.key)
+}
+
 </script>
 
 <style scoped lang=scss>
