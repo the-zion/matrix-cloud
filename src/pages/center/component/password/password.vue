2ca92a0e11c8a13f5247fe600e7871fa48c774af
liusiyuan <945212191@qq.com>
2022-05-15 23:36:27 +0800

    feat(pages.center): center模块新增子组件password

---
+++ src/pages/center/component/password/password.vue
@@ -0,0 +1,83 @@
+<template>
+  <el-container class="password">
+    <el-form :model="form" ref="formRef" class="form" :rules="rules">
+      <el-form-item class="form-item" prop="password">
+        <el-input v-model="form.password" :maxlength="50" placeholder="请输入密码"/>
+      </el-form-item>
+      <el-form-item class="form-item" prop="repeat">
+        <el-input v-model="form.repeat" :maxlength="50" placeholder="请重复输入密码"/>
+      </el-form-item>
+    </el-form>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: "MatrixSecurityPassword"
+}
+</script>
+
+<script setup>
+import {ref, onMounted} from "vue";
+
+import {validatePassword} from "../../../../utils/check";
+
+const emits = defineEmits(["open"])
+const rules = ref({
+  password: [{validator: validatePassword, trigger: 'blur'}],
+  repeat: [{validator: validateRepeat, trigger: 'blur'}]
+})
+
+let form = ref({password: "", repeat: ""})
+let formRef = ref()
+
+function validateRepeat(rule, value, callback) {
+  if (!checkRepeat(value)) {
+    value || callback(new Error("密码不能为空"))
+    callback(new Error("两次密码输入不一致"))
+  }
+  callback()
+}
+
+function checkRepeat(value) {
+  return value === form.value.password
+}
+
+onMounted(function () {
+  emits("open", form.value, formRef.value)
+})
+</script>
+
+<style scoped lang="scss">
+.password {
+  width: 100%;
+
+  .form {
+    width: 100%;
+
+    .form-item {
+      width: 100%;
+      margin-bottom: 30px;
+
+      .code {
+        width: 100%;
+
+        .code-input {
+          width: calc(100% - 100px);
+        }
+
+        .button {
+          height: 42px;
+          padding: 12px 0;
+          width: 90px;
+          text-align: center;
+        }
+      }
+
+      ::v-deep(.el-input__wrapper) {
+        height: 40px;
+      }
+    }
+  }
+}
+</style>
