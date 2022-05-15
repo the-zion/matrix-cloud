ac6958737d41bdf4bbaaff327a9408612cda23b0
liusiyuan <945212191@qq.com>
2022-05-15 23:41:29 +0800

    feat(pages.home): home模块子页面blog新增

---
+++ src/pages/home/blog/blog.vue
@@ -0,0 +1,80 @@
+<template>
+  <el-container class="blog">
+    <el-row class="head" justify="space-between">
+      <el-space class="menu" :size="32">
+        <el-row class="each" :class="{'menu-select':current === item.key}" v-for="item in menu"
+                @click="current = item.key" align="middle">
+          {{ item.label }}
+        </el-row>
+      </el-space>
+      <el-space class="operate">
+        <el-input
+            class="input"
+            v-model="input"
+            placeholder="题目/标签/作者/内容"
+            suffix-icon="Search"
+        />
+        <el-button round type="primary" icon="EditPen">博客撰写</el-button>
+      </el-space>
+    </el-row>
+    <matrix-list :mode="1" shape="card" :gap="10" :tag="true" :pageBackground="true"></matrix-list>
+  </el-container>
+</template>
+
+<script setup>
+import {ref} from "vue"
+
+let input = ref()
+let current = ref("hot")
+let menu = ref([
+  {
+    key: "hot",
+    label: "最热"
+  }, {
+    key: "new",
+    label: "最新"
+  }, {
+    key: "collect",
+    label: "收藏最多"
+  }, {
+    key: "view",
+    label: "浏览最多"
+  }
+])
+</script>
+
+<style scoped lang="scss">
+.blog {
+  width: 100%;
+  flex-direction: column;
+
+  .head {
+    width: 100%;
+    margin-bottom: 14px;
+
+    .menu {
+      .each {
+        color: var(--el-text-color-secondary);
+        cursor: pointer;
+        height: 32px;
+      }
+
+      .menu-select {
+        font-weight: 500;
+        color: var(--el-text-color-primary);
+        border-bottom: 2px solid var(--el-text-color-primary);
+      }
+    }
+
+    .operate {
+      .input {
+        width: 250px;
+      }
+    }
+  }
+
+  .body {
+    width: 100%;
+  }
+}
+</style>
