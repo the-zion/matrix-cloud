fba9e73f667408c55f4ff6b245bfb3616593cf7d
liusiyuan <945212191@qq.com>
2022-05-16 23:58:48 +0800

    refactor(App): body样式重构

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <router-view v-slot="{ Component }">
-    <transition name="app" mode="out-in">
-      <component :is="Component"/>
-    </transition>
+    <component :is="Component"/>
   </router-view>
 
 </template>
@@ -10,27 +8,30 @@
 <script setup>
 </script>
 <style lang="scss">
-::-webkit-scrollbar {
-  width: 6px;
-  display: none
-}
-
-.el-loading-parent--relative {
-  position: unset !important;
-}
 
 body {
   margin: unset;
-  overflow: hidden;
+  overflow-y: scroll;
   font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB',
   'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
-}
 
-.app-enter-active, .app-leave-active {
-  transition: opacity 0.3s linear;
-}
+  .dropdown {
+    border-radius: 0.5rem;
 
-.app-enter-from, .app-leave-to {
-  opacity: 0;
+    .el-popper__arrow {
+      display: none;
+    }
+  }
+
+  .matrix-message-box {
+    vertical-align: unset;
+    border-radius: 8px;
+  }
+
+  .popover {
+    padding: 16px 0 !important;
+    border-radius: 10px !important;
+  }
+
 }
 </style>
