63a0d7f49ac5c6609914f137b613e7755134ba4d
liusiyuan <945212191@qq.com>
2022-05-17 23:30:02 +0800

    refactor(component): list组件代码重构

--- src/components/blog/blog.vue
+++ src/components/blog/blog.vue
@@ -2,13 +2,18 @@
   <el-row class="blog">
     <el-space class="main" fill>
       <el-space class="head">
-        <el-avatar class="avatar" :size="24" :src="props.data.avatar"/>
+        <el-popover placement="top-start" :show-arrow="false" :width="312" trigger="hover" popper-class="popover">
+          <template #reference>
+            <el-avatar @click="GotoUserPage(1)" class="avatar" :size="24" :src="props.data.avatar"/>
+          </template>
+          <matrix-card></matrix-card>
+        </el-popover>
         <el-row class="title">{{ props.data.title }}</el-row>
       </el-space>
       <el-space class="info">
         <el-row class="name">{{ props.data.name }}</el-row>
         <el-row class="time">{{ "发布于 " + props.data.time }}</el-row>
-        <el-tag v-show="props.data.tags" type="info" v-for="tag in props.data.tags.split(';')">{{ tag }}</el-tag>
+        <el-tag round v-show="props.data.tags" type="info" v-for="tag in props.data.tags.split(';')">{{ tag }}</el-tag>
       </el-space>
       <el-space class="body">
         <el-image class="image" fit="cover" :src="props.data.image" lazy></el-image>
@@ -18,19 +23,30 @@
     <el-space class="foot">
       <el-space :size="3">
         <el-icon class="iconfont icon-like icon"></el-icon>
-        <span class="num">{{ props.data.agree > 1000 ? (props.data.agree / 1000).toFixed(1) + "k" : props.data.agree }}</span>
+        <span class="num">{{
+            props.data.agree > 1000 ? (props.data.agree / 1000).toFixed(1) + "k" : props.data.agree
+          }}</span>
       </el-space>
       <el-space :size="3">
         <el-icon class="iconfont icon-eye icon"></el-icon>
-        <span class="num">{{ props.data.view > 1000 ? (props.data.view / 1000).toFixed(1) + "k" : props.data.view}}</span>
+        <span
+            class="num">{{
+            props.data.view > 1000 ? (props.data.view / 1000).toFixed(1) + "k" : props.data.view
+          }}</span>
       </el-space>
       <el-space :size="3">
         <el-icon class="iconfont icon-message icon"></el-icon>
-        <span class="num">{{ props.data.comment > 1000 ? (props.data.comment / 1000).toFixed(1) + "k" : props.data.comment}}</span>
+        <span
+            class="num">{{
+            props.data.comment > 1000 ? (props.data.comment / 1000).toFixed(1) + "k" : props.data.comment
+          }}</span>
       </el-space>
       <el-space :size="3">
         <el-icon class="iconfont icon-star icon"></el-icon>
-        <span class="num">{{ props.data.collect > 1000 ? (props.data.collect / 1000).toFixed(1) + "k" : props.data.collect}}</span>
+        <span
+            class="num">{{
+            props.data.collect > 1000 ? (props.data.collect / 1000).toFixed(1) + "k" : props.data.collect
+          }}</span>
       </el-space>
     </el-space>
   </el-row>
@@ -44,6 +60,7 @@ export default {
 
 <script setup>
 import {ref} from "vue";
+import {GotoUserPage} from "../../utils/globalFunc";
 
 const props = defineProps({
   data: Object,
@@ -88,6 +105,10 @@ let data = ref({
         font-size: 16px;
         font-weight: 500;
         color: var(--el-text-color-primary)
+      }
+
+      .title:hover {
+        color: var(--el-color-primary);
       }
     }
 
