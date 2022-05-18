4d162fb6b8f1d009cf87500c98a6b1e9a41718c2
liusiyuan <945212191@qq.com>
2022-05-18 23:14:40 +0800

    feat(utils.tag): 新增全局标签字段

---
+++ src/utils/tag.js
@@ -0,0 +1,27 @@
+export function getTags() {
+    return [{
+        label: "前端/移动",
+        children: ["Node.js", "网络", "webpack", "音视频", "浏览器", "Swift", "Kotlin", "iOS", "Android", "框架", "TypeScript", "JavaScript", "可视化"]
+    }, {
+        label: "计算机基础",
+        children: ["组成原理", "数学", "代码", "编译原理", "操作系统", "数据库", "工具", "网络", "算法"]
+    }, {
+        label: "后端/架构",
+        children: [".NET", "C语言", "Java", "Python", "Go", "基本功", "分布式", "中间件", "区块链", "全栈", "C++", "中台", "DDD", "案例", "微服务", "数据库"]
+    }, {
+        label: "AI/大数据",
+        children: ["数据分析", "NLP", "大数据", "推荐系统", "AI算法", "数学", "机器学习"]
+    }, {
+        label: "运维/测试",
+        children: ["云计算", "敏捷开发", "性能", "Linux", "测试", "安全", "CI/CD", "运维管理", "DevOps", "Kubernetes"]
+    }, {
+        label: "产品/运营",
+        children: ["市场", "产品创新", "增长", "运营"]
+    }, {
+        label: "管理/成长",
+        children: ["项目管理", "个人成长", "团队管理", "研发效能", "OKR"]
+    }, {
+        label: "人文/兴趣",
+        children: ["短视频", "英语", "写作", "摄影", "音乐", "生活", "萌宠"]
+    }]
+}
