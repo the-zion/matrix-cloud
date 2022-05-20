5bc6d3ba18d07caec3c942e40d199c7c9d805d08
liusiyuan <945212191@qq.com>
2022-05-20 23:49:38 +0800

    refactor(App): 组件按需加载，body样式分离

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,8 @@
 <template>
-  <router-view v-slot="{ Component }">
-    <component :is="Component"/>
-  </router-view>
-
+  <el-config-provider>
+    <router-view v-slot="{ Component }">
+      <component :is="Component"/>
+    </router-view>
+  </el-config-provider>
 </template>
 
-<script setup>
-</script>
-<style lang="scss">
-
-:root{
-  --el-overlay-color-lighter: rgba(0, 0, 0, 0.2) !important;
-}
-
-body {
-  margin: unset;
-  overflow: auto;
-  font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB',
-  'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
-
-  .dropdown {
-    border-radius: 0.5rem;
-
-    .el-popper__arrow {
-      display: none;
-    }
-  }
-
-  .matrix-message-box {
-    vertical-align: unset;
-    border-radius: 8px;
-  }
-
-  .popover {
-    padding: 16px 0 !important;
-    border-radius: 10px !important;
-  }
-
-}
-</style>
