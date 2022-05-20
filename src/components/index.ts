ff413784fc44c766d42023207611bc0cd6daa3c6
liusiyuan <945212191@qq.com>
2022-05-20 23:52:24 +0800

    refactor(component): 组件更新

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,20 +1,10 @@
 import {App} from 'vue'
 import MatrixList from './list/list.vue'
-import MatrixBlog from './blog/blog.vue'
-import MatrixColumn from './column/column.vue'
-import MatrixTalk from './talk/talk.vue'
 import MatrixCard from './card/card.vue'
-import MatrixTag from './tag/tag.vue'
-import MatrixDraft from './draft/draft.vue'
 
 export default {
     install(app: App) {
         app.component(MatrixList.name, MatrixList)
-        app.component(MatrixBlog.name, MatrixBlog)
-        app.component(MatrixColumn.name, MatrixColumn)
-        app.component(MatrixTalk.name, MatrixTalk)
         app.component(MatrixCard.name, MatrixCard)
-        app.component(MatrixTag.name, MatrixTag)
-        app.component(MatrixDraft.name, MatrixDraft)
     }
 }
