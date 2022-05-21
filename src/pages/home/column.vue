a42fca8cbc94b1bb1df9e8b04606a811c4469bac
liusiyuan <945212191@qq.com>
2022-05-21 16:45:24 +0800

    feat(pages.home): add 标签过滤

--- src/pages/home/column.vue
+++ src/pages/home/column.vue
@@ -18,14 +18,49 @@
         <el-button round type="primary" icon="Plus" @click="visible = true">专栏创建</el-button>
       </el-space>
     </el-row>
-    <matrix-list :mode="2" shape="card" :gap="10" :tag="true" :pageBackground="true"
-                 @current-page="pageChange"></matrix-list>
+    <el-row class="tag-block" align="middle" justify="space-between">
+      <el-space class="tags">
+        <el-tag
+            style="cursor: pointer"
+            v-for="item in tags"
+            :key="item.label"
+            :effect="item.select?'dark':'light'"
+            round
+            @click="selectTag(item)"
+        >
+          {{ item.label }}
+        </el-tag>
+      </el-space>
+      <el-space :size="3" class="tag-filter" @click="tagFilter = true">
+        <el-icon>
+          <Filter/>
+        </el-icon>
+        <span>筛选器</span>
+      </el-space>
+      <el-dialog
+          v-model="tagFilter"
+          destroy-on-close
+          :width="480"
+          custom-class="tag-filter-dialog"
+          @close="tagFilter = false"
+
+      >
+        <matrix-tag v-model:selectedTags="selectedTags"></matrix-tag>
+        <template #footer>
+          <span>
+            <el-button @click="tagFilter = false" round>取消</el-button>
+            <el-button @click="filterByTags" round type="primary"
+            >确定</el-button>
+          </span>
+        </template>
+      </el-dialog>
+    </el-row>
+    <matrix-list component="MatrixColumnCard" shape="card" :gap="10" :page-background="true"></matrix-list>
   </el-container>
 </template>
 
 <script setup>
 import {ref} from "vue"
-import {scrollToTop} from "../../utils/scroll";
 
 let input = ref()
 let visible = ref(false)
@@ -46,27 +81,39 @@ let menu = ref([
   }
 ])
 
-function pageChange() {
-  scrollToTop()
+let tagFilter = ref(false)
+let tags = ref([{
+  label: "go",
+  select: false
+}, {
+  label: "k8s",
+  select: false
+}, {
+  label: "云原生",
+  select: false
+}, {
+  label: "python",
+  select: false
+}, {
+  label: "docker",
+  select: false
+}])
+let selectedTags = ref([])
+
+function selectTag(each) {
+  each.select = !each.select
 }
+
+function filterByTags() {
+
+}
+
 </script>
 
 <style scoped lang="scss">
 .column {
   width: 100%;
   flex-direction: column;
-
-  //::v-deep(.create-dialog){
-  //  margin: auto;
-  //  height: 100%;
-  //  .el-dialog__header{
-  //    display: none;
-  //  }
-  //  .el-dialog__body{
-  //    padding: 15px;
-  //    height: calc(100% - 30px);
-  //  }
-  //}
 
   .head {
     width: 100%;
@@ -89,6 +136,37 @@ function pageChange() {
     .operate {
       .input {
         width: 250px;
+      }
+    }
+  }
+
+  .tag-block {
+    width: 100%;
+    margin-bottom: 14px;
+
+    .tags {
+      width: calc(100% - 65px);
+    }
+
+    .tag-filter {
+      font-size: 14px;
+      color: var(--el-text-color-secondary);
+      cursor: pointer;
+    }
+
+    .tag-filter:hover {
+      color: var(--el-color-primary);
+    }
+
+    ::v-deep(.tag-filter-dialog) {
+      border-radius: 8px;
+
+      .el-dialog__header {
+        display: none;
+      }
+
+      .el-dialog__body {
+        padding: 20px 20px 0 20px
       }
     }
   }
