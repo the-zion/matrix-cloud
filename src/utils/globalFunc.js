f6a1761f7a96be80046e4b69259910e7271f9013
liusiyuan <945212191@qq.com>
2022-05-22 23:39:53 +0800

    refactor(utils): 全局函数更新

--- src/utils/globalFunc.js
+++ src/utils/globalFunc.js
@@ -1,96 +1,5 @@
-import {ElLoading, ElMessageBox} from 'element-plus'
+import {ElMessageBox} from 'element-plus'
 import router from "../router";
-
-export function globalFunc() {
-
-    function loadFullScreen() {
-        return ElLoading.service({
-            text: "Loading...",
-            lock: true,
-        })
-    }
-
-    function getClassList() {
-        return [{
-            "value": "language",
-            "label": "编程语言",
-            "children": [{
-                "value": "python",
-                "label": "Python",
-            }, {
-                "value": "go",
-                "label": "Go",
-            }, {
-                "value": "java",
-                "label": "Java",
-            }, {
-                "value": "javaScript",
-                "label": "JavaScript",
-            }, {
-                "value": "c++",
-                "label": "C++",
-            }, {
-                "value": "c",
-                "label": "C",
-            }],
-        }, {
-            "value": "middleware",
-            "label": "中间件",
-            "children": [{
-                "value": "redis",
-                "label": "Redis",
-            }, {
-                "value": "rabbitmq",
-                "label": "Rabbitmq",
-            }]
-        }, {
-            "value": "virtualization",
-            "label": "云原生",
-            "children": [{
-                "value": "docker",
-                "label": "Docker",
-            }, {
-                "value": "kubernetes",
-                "label": "kubernetes",
-            }, {
-                "value": "microServices",
-                "label": "微服务",
-            }]
-        }, {
-            "value": "database",
-            "label": "数据库",
-            "children": [{
-                "value": "mysql",
-                "label": "Mysql",
-            }]
-        }, {
-            "value": "basics",
-            "label": "计算机基础",
-            "children": [{
-                "value": "network",
-                "label": "网络",
-            }, {
-                "value": "dataStructure",
-                "label": "数据结构和算法",
-            }, {
-                "value": "operatingSystem",
-                "label": "操作系统",
-            }, {
-                "value": "computerComposition",
-                "label": "计算机组成原理",
-            }]
-        }, {
-            "value": "other",
-            "label": "其他",
-            "children": [{
-                "value": "live",
-                "label": "生活",
-            }]
-        }]
-    }
-
-    return {loadFullScreen, getClassList}
-}
 
 export function confirm(title, message, type) {
     return ElMessageBox.confirm(
@@ -105,10 +14,10 @@ export function confirm(title, message, type) {
     )
 }
 
-export function GotoUserPage(id) {
-    const params = { id: id };
+export function goToPage(state, id) {
+    const params = {id: id};
     const {href} = router.resolve({
-        name: "user",
+        name: state,
         query: params
     });
     window.open(href, "_blank");
