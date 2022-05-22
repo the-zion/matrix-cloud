be3bd37d6121404226f8b5fe0eedc75a2df4f001
liusiyuan <945212191@qq.com>
2022-05-22 23:45:34 +0800

    feat(pages.talk): add comment

---
+++ src/pages/talk/component/comment.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-row class="talk-common">
+    <span>hello</span>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: "Comment"
+}
+</script>
+
+<style scoped lang="scss">
+.talk-common {
+  width: 100%;
+}
+</style>
