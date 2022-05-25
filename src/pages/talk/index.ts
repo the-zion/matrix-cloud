3afff9d81924de2a725af9d07249a9d114dec572
liusiyuan <945212191@qq.com>
2022-05-25 23:04:38 +0800

    refactor(pages.talk): remove talk-create

--- src/pages/talk/index.ts
+++ src/pages/talk/index.ts
@@ -1,11 +1,9 @@
 import {App} from 'vue'
 
 import MatrixTalkList from './component/list.vue'
-import MatrixTalkCreate from './component/create.vue'
 
 export default {
     install(app: App) {
         app.component(MatrixTalkList.name, MatrixTalkList)
-        app.component(MatrixTalkCreate.name, MatrixTalkCreate)
     }
 }
