c77a3aeb069b6ad6cce04795ce82c5292343849e
liusiyuan <945212191@qq.com>
2022-05-25 23:12:45 +0800

    feat(pages.user): add list

--- src/pages/user/index.ts
+++ src/pages/user/index.ts
@@ -1,8 +1,10 @@
 import {App} from 'vue'
 import MatrixUserMiniCard from './component/mini.vue'
+import MatrixUserList from './component/list.vue'
 
 export default {
     install(app: App) {
         app.component(MatrixUserMiniCard.name, MatrixUserMiniCard)
+        app.component(MatrixUserList.name, MatrixUserList)
     }
 }
