86cdde8d032e88bf13ad1fe8a17a211b23c8f48a
liusiyuan <945212191@qq.com>
2022-05-25 23:11:26 +0800

    refactor(pages.column): remove column-create

--- src/pages/column/index.ts
+++ src/pages/column/index.ts
@@ -1,11 +1,9 @@
 import {App} from 'vue'
 
 import MatrixColumnList from './component/list.vue'
-import MatrixColumnCreate from './component/create.vue'
 
 export default {
     install(app: App) {
         app.component(MatrixColumnList.name, MatrixColumnList)
-        app.component(MatrixColumnCreate.name, MatrixColumnCreate)
     }
 }
