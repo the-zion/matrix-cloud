a28f0b6a800da0a34ed6d7002af9e6e68f116c12
liusiyuan <945212191@qq.com>
2022-06-13 23:37:50 +0800

    refactor: change vuex to pinia

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import App from './App.vue'
 import {createApp} from 'vue'
+import {createPinia} from 'pinia'
 import ReplyComponent from './pages/reply/index.ts'
 import CommentComponent from './pages/comment/index.ts'
 import BlogComponents from './pages/blog/index.ts'
@@ -8,7 +9,6 @@ import ColumnComponents from './pages/column/index.ts'
 import TagComponents from './pages/tag/index.ts'
 import UserComponent from './pages/user/index.ts'
 import router from './router/index'
-import store from "./store/index"
 import * as ElIconModules from '@element-plus/icons-vue'
 import './assets/icon/iconfont.css'
 import 'element-plus/dist/index.css'
@@ -16,8 +16,8 @@ import '@wangeditor/editor/dist/css/style.css'
 import "~/styles/index.scss";
 import 'uno.css'
 
-
-const app = createApp(App).use(store)
+const pinia = createPinia()
+const app = createApp(App).use(pinia)
     .use(ReplyComponent)
     .use(CommentComponent)
     .use(BlogComponents)
