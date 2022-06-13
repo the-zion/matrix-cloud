f72c9024df27c70881b0dae16b2b35ad8d3c5b4f
liusiyuan <945212191@qq.com>
2022-06-13 23:33:33 +0800

    refactor: update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,56 +1,55 @@
 import path from 'path'
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 import Unocss from 'unocss/vite'
 import {
-  presetAttributify,
-  presetIcons,
-  presetUno,
-  transformerDirectives,
-  transformerVariantGroup,
+    presetAttributify,
+    presetIcons,
+    presetUno,
+    transformerDirectives,
+    transformerVariantGroup,
 } from 'unocss'
 
 const pathSrc = path.resolve(__dirname, 'src')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '~/': `${pathSrc}/`,
+    resolve: {
+        alias: {
+            '~/': `${pathSrc}/`,
+        },
     },
-  },
-  plugins: [
-    vue(),
-    Components({
-      extensions: ['vue', 'md'],
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      resolvers: [
-        ElementPlusResolver({
-          importStyle: 'sass',
+    plugins: [
+        vue(),
+        Components({
+            extensions: ['vue', 'md'],
+            include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+            resolvers: [
+                ElementPlusResolver({
+                    importStyle: 'sass',
+                }),
+            ],
+            dts: 'src/components.d.ts',
         }),
-      ],
-      dts: 'src/components.d.ts',
-    }),
 
-    // https://github.com/antfu/unocss
-    // see unocss.config.ts for config
-    Unocss({
-      presets: [
-        presetUno(),
-        presetAttributify(),
-        presetIcons({
-          scale: 1.2,
-          warn: true,
+        // https://github.com/antfu/unocss
+        // see unocss.config.ts for config
+        Unocss({
+            presets: [
+                presetUno(),
+                presetAttributify(),
+                presetIcons({
+                    scale: 1.2,
+                    warn: true,
+                }),
+            ],
+            transformers: [
+                transformerDirectives(),
+                transformerVariantGroup(),
+            ]
         }),
-      ],
-      transformers: [
-        transformerDirectives(),
-        transformerVariantGroup(),
-      ]
-    }),
-  ],
+    ],
 })
