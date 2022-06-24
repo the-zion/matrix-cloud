1818bd209a4e3b9583400ecb8288c3370bf42e01
liusiyuan <945212191@qq.com>
2022-06-24 23:12:53 +0800

    refactor: update

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,53 @@
 import {defineStore} from "pinia"
+import {get} from "../utils/axios"
+import {warning} from "../utils/message";
 
-export const store = defineStore("main",{
-    state: ()=>{
-        return {}
+export const userMainStore = defineStore("user", {
+    state: () => {
+        return {
+            uuid: null,
+            username: null,
+            avatar: null,
+        }
     },
 
-    getters:{
+    getters: {},
 
-    },
-
-    actions:{
-
+    actions: {
+        getUserProfile() {
+            let that = this
+            get("/v1/get/user/profile").then(function (reply) {
+                that.$patch(state => {
+                    state.uuid = reply.data.uuid
+                    state.username = reply.data.username
+                    state.avatar = reply.data.avatar
+                })
+            }).catch(function (err) {
+                let response = err.response
+                if (response) {
+                    switch (response.data.reason) {
+                        case "TOKEN_EXPIRED":
+                            warning("登录已过期，请重新登录")
+                            localStorage.removeItem("matrix-token")
+                            break
+                    }
+                }
+                that.$reset()
+            })
+        }
     }
+})
+
+export const baseMainStore = defineStore("base", {
+    state: () => {
+        return {
+            endpoint: "http://127.0.0.1:8080",
+            avatar: {
+                region: "ap-shanghai",
+                bucket: "user-1309416642",
+                key: "avatar/",
+                baseUrl: "https://user-1309416642.cos.ap-shanghai.myqcloud.com/avatar/"
+            }
+        }
+    },
 })
