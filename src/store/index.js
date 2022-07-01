27fb48876555475aa4eddaa031b30c3e87c1dc79
liusiyuan <945212191@qq.com>
2022-07-01 23:55:49 +0800

    refactor: update

--- src/store/index.js
+++ src/store/index.js
@@ -47,6 +47,12 @@ export const baseMainStore = defineStore("base", {
                 bucket: "user-1309416642",
                 key: "avatar/",
                 baseUrl: "https://user-1309416642.cos.ap-shanghai.myqcloud.com/avatar/"
+            },
+            article: {
+                region: "ap-shanghai",
+                bucket: "creation-1309416642",
+                key: "article/",
+                baseUrl: "https://creation-1309416642.cos.ap-shanghai.myqcloud.com/article/"
             }
         }
     },
