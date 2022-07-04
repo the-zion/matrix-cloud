cbbb63427ec7cf8b322b6043b004a5d8c3bbf4ea
liusiyuan <945212191@qq.com>
2022-07-04 23:59:38 +0800

    refactor: update

--- src/utils/cos.js
+++ src/utils/cos.js
@@ -4,19 +4,15 @@ import {error} from "./message";
 
 export function initCos() {
     return new COS({
-        // getAuthorization 必选参数
         getAuthorization: function (options, callback) {
-            // 服务端例子：https://github.com/tencentyun/qcloud-cos-sts-sdk/blob/master/scope.md
-            // 异步获取临时密钥
             get("/v1/get/cos/session/key").then(function (reply) {
                 let data = reply.data
                 callback({
                     TmpSecretId: data["tmpSecretId"],
                     TmpSecretKey: data["tmpSecretKey"],
                     SecurityToken: data["sessionToken"],
-                    // 建议返回服务器时间作为签名的开始时间，避免用户浏览器本地时间偏差过大导致签名错误
-                    StartTime: data["startTime"], // 时间戳，单位秒，如：1580000000
-                    ExpiredTime: data["expiredTime"], // 时间戳，单位秒，如：1580000000
+                    StartTime: data["startTime"],
+                    ExpiredTime: data["expiredTime"],
                 });
             }).catch(function () {
                 error("图片上传失败，请稍后再试")
