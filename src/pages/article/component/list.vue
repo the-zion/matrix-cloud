d814ac3c34c27f4b10203de46269156e569ffca4
liusiyuan <945212191@qq.com>
2022-07-05 23:57:48 +0800

    refactor: update

--- src/pages/article/component/list.vue
+++ src/pages/article/component/list.vue
@@ -1,61 +1,64 @@
 <template>
   <el-row class="blog-list" id="blog-list">
-    <el-empty v-show="data.length === 0" class="empty" description=" "
+    <el-empty v-show="data.length === 0 && !loading" class="empty" description=" "
               :image-size="250" image="../../src/assets/images/no_data.svg"
     />
-    <el-space class="data" fill :size="props.gap || 0">
-      <el-row v-for="item in data" class="each" :class="props.shape" :key="item.id"
-              @click="goToPage('blog', item.id)">
+    <el-row class="data" fill :size="props.gap || 0">
+      <el-row v-for="item in data" class="each" :key="item.id"
+              @click="goToPage('article', item.id)">
         <el-row class="blog-card">
           <el-space class="main" fill>
             <el-space class="head">
               <el-popover placement="top-start" :show-arrow="false" :width="312" trigger="hover" popper-class="popover">
                 <template #reference>
-                  <el-avatar @click="goToPage('user', 1)" class="avatar" :size="24" :src="item.avatar"/>
+                  <el-avatar @click="goToPage('user', 1)" class="avatar" :size="24"
+                             :src="avatar.baseUrl + item.uuid + '.webp'"/>
                 </template>
                 <matrix-user-mini-card></matrix-user-mini-card>
               </el-popover>
-              <el-row class="title">{{ item.title }}</el-row>
+              <el-row class="title">{{ introduce[item.id].title }}</el-row>
             </el-space>
             <el-space class="info">
-              <el-row class="name">{{ item.name }}</el-row>
-              <el-row class="time">{{ "发布于 " + item.time }}</el-row>
-              <el-tag round v-show="item.tags" type="info" v-for="tag in item.tags.split(';')" :key="tag">{{
+              <!--              <el-row class="name">{{ item.name }}</el-row>-->
+              <el-row class="time">{{ "发布于 " + introduce[item.id].update }}</el-row>
+              <el-tag round v-show="introduce[item.id].tags" type="info"
+                      v-for="tag in introduce[item.id].tags.split(';')" :key="tag">{{
                   tag
                 }}
               </el-tag>
             </el-space>
-            <el-space class="body">
-              <el-image v-show="item.image" class="image" fit="cover" :src="item.image" lazy></el-image>
-              <span class="content">{{ item.content }}</span>
+            <el-space class="body" alignment="flex-start">
+              <el-image v-if="introduce[item.id].cover" class="image" fit="cover" :src="introduce[item.id].cover"
+                        lazy></el-image>
+              <span class="content">{{ introduce[item.id].text }}</span>
             </el-space>
           </el-space>
           <el-space class="foot">
             <el-space :size="3">
               <el-icon class="iconfont icon-like icon"></el-icon>
               <span class="num">{{
-                  item.agree > 1000 ? (item.agree / 1000).toFixed(1) + "k" : item.agree
+                  count[item.id].agree > 1000 ? (count[item.id].agree / 1000).toFixed(1) + "k" : count[item.id].agree
                 }}</span>
             </el-space>
             <el-space :size="3">
               <el-icon class="iconfont icon-eye icon"></el-icon>
               <span
                   class="num">{{
-                  item.view > 1000 ? (item.view / 1000).toFixed(1) + "k" : item.view
+                  count[item.id].view > 1000 ? (count[item.id].view / 1000).toFixed(1) + "k" : count[item.id].view
                 }}</span>
             </el-space>
             <el-space :size="3">
               <el-icon class="iconfont icon-message icon"></el-icon>
               <span
                   class="num">{{
-                  item.comment > 1000 ? (item.comment / 1000).toFixed(1) + "k" : item.comment
+                  count[item.id].comment > 1000 ? (count[item.id].comment / 1000).toFixed(1) + "k" : count[item.id].comment
                 }}</span>
             </el-space>
             <el-space :size="3">
               <el-icon class="iconfont icon-star icon"></el-icon>
               <span
                   class="num">{{
-                  item.collect > 1000 ? (item.collect / 1000).toFixed(1) + "k" : item.collect
+                  count[item.id].collect > 1000 ? (count[item.id].collect / 1000).toFixed(1) + "k" : count[item.id].collect
                 }}</span>
             </el-space>
           </el-space>
@@ -69,17 +72,8 @@
           </div>
         </el-space>
       </el-row>
-    </el-space>
-    <el-row class="foot" justify="center" v-show="data.length !== 0">
-      <el-pagination
-          :background="props.pageBackground"
-          v-model:current-page="currentPage"
-          :page-size="20"
-          :pager-count="11"
-          layout="prev, pager, next"
-          :total="1000"
-      />
     </el-row>
+    <el-skeleton class="skeleton" v-show="loading" :rows="3" animated/>
   </el-row>
 </template>
 
@@ -91,52 +85,103 @@ export default {
 
 <script setup>
 import {ref, watch, onMounted} from "vue";
+import {throttle, scrollToBottomListen} from "../../../utils/scroll";
 import {goToPage} from "../../../utils/globalFunc";
 import {confirm} from "../../../utils/globalFunc";
-import {success} from "../../../utils/message";
-import {scrollTo} from "../../../utils/scroll";
+import {error, info, success} from "../../../utils/message";
+import {get} from "../../../utils/axios";
+import {baseMainStore} from "../../../store";
+import {storeToRefs} from "pinia/dist/pinia.esm-browser";
 
 const emits = defineEmits(["current-page"])
 const props = defineProps({
-  gap: Number,
-  shape: String,
   operation: {
     type: Array,
     default: []
   },
-  "page-background": Boolean,
 })
+const baseStore = baseMainStore()
+const {avatar, article} = storeToRefs(baseStore)
 
 let data = ref([])
+let count = ref({})
+let introduce = ref({})
 let currentPage = ref(1)
+let loading = ref(false)
+let isBottom = false
+let request = 0
+
 
 function init() {
-  initData()
+  scrollToBottomListen(throttle(scrollToBottom, 1000))
   getData()
 }
 
-function initData() {
+function scrollToBottom() {
+  getData()
 }
 
+
 function getData() {
-  let d = []
-  for (let i = 0; i <= 9; i++) {
-    d.push({
-      id: i,
-      title: "迭代法实现二叉树的前序、中序和后序遍历" + i,
-      avatar: "../../src/assets/images/boy.png",
-      image: "../../src/assets/images/img.png",
-      name: "刘小圆sama",
-      time: "2022-05-06",
-      content: "本文的目的是为了总结出一个通用的，改动较少的非递归模板，可以分别适用于二叉树的前序、中序和后续遍历，方便记忆。前序遍历前序遍历的遍历顺序为：根 -> 左 -> 右。只要有左子树，就把左子树入栈，同时把值加入答案数组，然后依次弹出栈顶元素，移动到它的右子树，重复操作。「代码块」中序遍历中序遍历的遍",
-      tags: "go;云原生",
-      agree: 10000,
-      view: 12000,
-      comment: 1100,
-      collect: 500
-    })
+  if (isBottom) {
+    info("到最底部啦～")
+    return
   }
-  data.value = d
+
+  loading.value = true
+  get("/v1/get/article/list?page=" + currentPage.value).then(function (reply) {
+    let list = reply.data.article
+    let size = list.length
+    if (size === 0) {
+      isBottom = true
+      info("到最底部啦～")
+      loading.value = false
+      return
+    }
+    request = size + 1
+    currentPage.value += 1
+    getStatistic(list)
+    getIntroduce(list)
+  })
+}
+
+function getStatistic(list) {
+  let ids = []
+  list.forEach(function (each) {
+    ids.push("ids=" + each.id)
+  })
+  get("/v1/get/article/list/statistic?" + ids.join("&")).then(function (reply) {
+    reply.data.count.forEach(function (each) {
+      count.value[each.id] = each
+    })
+    request -= 1
+  }).catch(function () {
+    request -= 1
+  }).then(function () {
+    if (request === 0) {
+      data.value = data.value.concat(list)
+      loading.value = false
+    }
+  })
+}
+
+
+function getIntroduce(list) {
+  let request = list.length
+  list.forEach(function (item) {
+    let url = article.value.baseUrl + item.id + "/" + item.uuid + "-introduce"
+    get(url).then(function (reply) {
+      introduce.value[item.id] = reply.data
+      request -= 1
+    }).catch(function () {
+      request -= 1
+    }).then(function () {
+      if (request === 0) {
+        data.value = data.value.concat(list)
+        loading.value = false
+      }
+    })
+  })
 }
 
 function doDelete(item) {
@@ -155,10 +200,6 @@ function doCollect(item) {
 
 defineExpose({
   getData
-})
-
-watch(currentPage, () => {
-  scrollTo("blog-list")
 })
 
 onMounted(() => {
@@ -228,7 +269,7 @@ onMounted(() => {
 
           .body {
             width: 100%;
-            height: 70px;
+            max-height: 80px;
 
             .image {
               height: 100%;
@@ -289,15 +330,17 @@ onMounted(() => {
       }
     }
 
-    .card {
-      border-radius: 8px;
+    .each:last-child {
       border-bottom: unset;
-      box-shadow: 0 1px 2px rgba(0, 10, 32, 0.1), 0 2px 8px rgba(0, 10, 32, 0.05);
     }
   }
 
-  .foot {
-    margin-top: 1rem;
+  .skeleton {
+    padding: 16px;
+  }
+
+  .list-foot {
+    margin: 1rem;
     width: 100%;
   }
 }
