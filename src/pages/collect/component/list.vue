f58f4a098d2f6ad3663f3e09805284019cbc8e6e
liusiyuan <945212191@qq.com>
2022-07-12 23:43:53 +0800

    refactor: update

--- src/pages/collect/component/list.vue
+++ src/pages/collect/component/list.vue
@@ -1,47 +1,55 @@
 <template>
-  <el-row class="blog-list" id="blog-list">
-    <el-empty v-show="data.length === 0" class="empty" description=" "
+  <el-row class="collect-list" id="collect-list">
+    <collections-create v-model:visible="visible" v-bind:mode="'edit'" v-bind:id="collectionsId"
+                        @editAfter="editAfter"></collections-create>
+    <el-empty v-show="data.length === 0 && !loading" class="empty" description=" "
               :image-size="250" image="../../src/assets/images/no_data.svg"
     />
-    <el-space class="data" fill :size="props.gap || 0">
-      <el-row v-for="item in data" class="each" :class="props.shape" :key="item.id"
-              @click="goToPage('blog', item.id)">
-        <el-row class="blog-card">
+    <el-skeleton class="skeleton" v-show="loading" :rows="2" animated/>
+    <el-space class="data" fill :size="0">
+      <el-row v-for="item in data" class="each" :key="item.id"
+              @click="goToPage('collect', item.id)">
+        <el-row class="collect-card">
           <el-space class="main" fill>
             <el-space class="head">
-              <el-row class="title">{{ item.title }}</el-row>
+              <el-row class="title">{{ item.name }}</el-row>
             </el-space>
             <el-space class="body">
               <span class="content">{{ item.introduce }}</span>
             </el-space>
           </el-space>
-          <el-space class="foot">
-            <el-space :size="3">
-              <span class="label">文章:</span>
-              <span class="num">{{
-                  item.agree > 1000 ? (item.agree / 1000).toFixed(1) + "k" : item.agree
-                }}</span>
-            </el-space>
-          </el-space>
         </el-row>
-        <el-space class="operation" size="large">
-          <div v-for="op in props.operation" :key="op">
-            <el-icon v-show="op === 'delete'" class="icon" @click="doDelete(item)">
-              <delete/>
+        <el-space class="operation" size="large" :style="{'display':item['opShow'] || 'none'}">
+          <el-space :size="3" @click.stop="collectionsEdit(item)" class="op">
+            <el-icon>
+              <EditPen/>
             </el-icon>
-            <el-icon v-show="op === 'star'" class="iconfont icon-star-fill star" @click="doCollect(item)"></el-icon>
-          </div>
+            <span class="word">编辑</span>
+          </el-space>
+          <el-popconfirm title="确认删除该收藏集"
+                         confirm-button-text="确定"
+                         cancel-button-text="取消"
+                         @confirm="collectionsDelete(item)"
+                         @cancel="opCancel(item)"
+          >
+            <template #reference>
+              <el-space :size="3" @click.stop="item['opShow'] = 'inline-flex'" class="op">
+                <el-icon>
+                  <Delete/>
+                </el-icon>
+                <span class="word">删除</span>
+              </el-space>
+            </template>
+          </el-popconfirm>
         </el-space>
       </el-row>
     </el-space>
-    <el-row class="foot" justify="center" v-show="data.length !== 0">
+    <el-row class="foot" justify="center" v-if="data.length !== 0 || currentPage > 1">
       <el-pagination
-          :background="props.pageBackground"
           v-model:current-page="currentPage"
-          :page-size="20"
-          :pager-count="11"
+          :page-size="10"
           layout="prev, pager, next"
-          :total="1000"
+          :total="pageTotal"
       />
     </el-row>
   </el-row>
@@ -55,66 +63,140 @@ export default {
 
 <script setup>
 import {ref, watch, onMounted} from "vue";
-import {goToPage} from "../../../utils/globalFunc";
-import {confirm} from "../../../utils/globalFunc";
-import {success} from "../../../utils/message";
+import {goToPage, loginTimeOut} from "../../../utils/globalFunc";
+import {error, success} from "../../../utils/message";
 import {scrollTo} from "../../../utils/scroll";
+import {useRoute} from "vue-router";
+import {userMainStore} from "../../../store";
+import {storeToRefs} from "pinia/dist/pinia.esm-browser";
+import {post, get} from "../../../utils/axios";
+import CollectionsCreate from "./create.vue";
 
 const emits = defineEmits(["current-page"])
-const props = defineProps({
-  gap: Number,
-  shape: String,
-  operation: {
-    type: Array,
-    default: []
-  },
-  "page-background": Boolean,
-})
+const userStore = userMainStore()
+const {uuid} = storeToRefs(userStore)
 
+let visible = ref(false)
 let data = ref([])
 let currentPage = ref(1)
+let pageTotal = ref(1)
+let loading = ref(false)
+let userId = ref()
+let collectionsId = ref()
 
 function init() {
   initData()
   getData()
+  getDataCount()
 }
 
 function initData() {
+  userId.value = useRoute().query["id"]
 }
 
 function getData() {
-  let d = []
-  for (let i = 0; i <= 9; i++) {
-    d.push({
-      id: i,
-      title: "Test" + i,
-      introduce: "海纳百川，有容乃大",
-      agree: 10000,
-    })
+  if (!userId.value) {
+    return
   }
-  data.value = d
+  loading.value = true
+  data.value = []
+  if (userId.value === uuid.value) {
+    getCollections()
+  } else {
+    getCollectionsVisitor()
+  }
 }
 
-function doDelete(item) {
-  confirm("删除", "确定删除" + "：\"" + item.title + "\" 吗？").then(function () {
-    success("删除成功")
-  }).catch(() => {
+function getDataCount() {
+  if (!userId.value) {
+    return
+  }
+  if (userId.value === uuid.value) {
+    getCollectionsCount()
+  } else {
+    getCollectionsVisitorCount()
+  }
+}
+
+function getCollections() {
+  post("/v1/get/collections", {
+    page: currentPage.value
+  }).then(function (reply) {
+    data.value = reply.data.collections
+    loading.value = false
+  }).catch(function () {
+    error("获取收藏集失败")
   })
 }
 
-function doCollect(item) {
-  confirm("取消收藏", "确定取消收藏" + "：\"" + item.title + "\" 吗？").then(function () {
-    success("取消成功")
-  }).catch(() => {
+function getCollectionsVisitor() {
+  get("/v1/get/collections/visitor?uuid=" + userId.value + "&page=" + currentPage.value).then(function (reply) {
+    data.value = reply.data.collections
+    loading.value = false
+  }).catch(function () {
   })
+}
+
+function getCollectionsCount() {
+  post("/v1/get/collections/count", {}).then(function (reply) {
+    pageTotal.value = reply.data.count
+  })
+}
+
+function getCollectionsVisitorCount() {
+  get("/v1/get/collections/visitor/count?uuid=" + userId.value).then(function (reply) {
+    pageTotal.value = reply.data.count
+  })
+}
+
+function createAfter() {
+  currentPage.value = 1;
+  getData()
+  getDataCount()
+}
+
+function opCancel(item) {
+  setTimeout(function () {
+    item['opShow'] = 'none'
+  }, 400)
+}
+
+function collectionsEdit(item) {
+  visible.value = true
+  collectionsId.value = item.id
+}
+
+function collectionsDelete(item) {
+  console.log(item.id)
+  post("/v1/delete/collections", {
+    id: item.id
+  }).then(function () {
+    success("收藏集已删除")
+    getData()
+  }).catch(function (err) {
+    let response = err.response
+    if (response) {
+      switch (response.data.reason) {
+        case "COLLECTIONS_IS_NOT_EMPTY":
+          error("只允许删除空的收藏集")
+          return
+      }
+    }
+    error("收藏级删除失败")
+  })
+}
+
+function editAfter() {
+  getData()
 }
 
 defineExpose({
-  getData
+  createAfter
 })
 
 watch(currentPage, () => {
-  scrollTo("blog-list")
+  scrollTo("collect-list")
+  getData()
 })
 
 onMounted(() => {
@@ -123,11 +205,15 @@ onMounted(() => {
 </script>
 
 <style scoped lang="scss">
-.blog-list {
+.collect-list {
   width: 100%;
 
   .empty {
     width: 100%;
+  }
+
+  .skeleton {
+    padding: 16px;
   }
 
   .data {
@@ -139,7 +225,7 @@ onMounted(() => {
       background-color: var(--el-color-white);
       cursor: pointer;
 
-      .blog-card {
+      .collect-card {
         width: 100%;
         padding: 16px;
 
@@ -205,50 +291,36 @@ onMounted(() => {
             }
           }
         }
-
-        .foot {
-          width: 100%;
-          margin-top: 1rem;
-
-          .icon {
-            font-size: 18px;
-            color: var(--el-text-color-secondary)
-          }
-
-          .label, .num {
-            font-size: 14px;
-            color: var(--el-text-color-secondary)
-          }
-        }
       }
 
       .operation {
         position: absolute;
+        display: none;
         top: 16px;
         right: 0;
+        color: var(--el-text-color-placeholder);
+        cursor: pointer;
 
-        .icon {
-          color: var(--el-text-color-placeholder);
-          font-size: 20px;
-          cursor: pointer;
+        .op {
+          .icon {
+            font-size: 20px;
+          }
+
+          .word {
+            font-size: 14px;
+          }
         }
 
-        .star {
-          color: #ffa116;
-          font-size: 20px;
-          cursor: pointer;
-        }
-
-        .icon:hover {
+        .op:hover {
           color: var(--el-color-primary);
         }
       }
     }
 
-    .card {
-      border-radius: 8px;
-      border-bottom: unset;
-      box-shadow: 0 1px 2px rgba(0, 10, 32, 0.1), 0 2px 8px rgba(0, 10, 32, 0.05);
+    .each:hover {
+      .operation {
+        display: inline-flex !important;
+      }
     }
   }
 
