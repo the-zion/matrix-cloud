778beef20671bd5e06c789b30cd4e5f01a5ded87
liusiyuan <945212191@qq.com>
2022-08-03 23:54:52 +0800

    refactor: update

--- src/pages/talk/talk.vue
+++ src/pages/talk/talk.vue
@@ -1,23 +1,26 @@
 <template>
   <el-container class="talk-container">
     <el-backtop></el-backtop>
+    <collections-choose v-model:visible="collectionsVisible" v-model:judge="statisticJudge" :id="talkId"
+                        :uuid="authorUuid" :mode="'talk'"
+                        @collected="collected"></collections-choose>
     <el-row class="talk-main">
       <el-row class="talk-block">
         <el-row class="header">
           <el-row class="title-area" justify="space-between">
             <el-row class="title" align="top">
-              <el-avatar :size="32" :src="data.image" class="avatar" @click="goToPage('user', 1)"></el-avatar>
+              <el-avatar :size="32" :src="avatar.baseUrl + authorUuid + '/avatar.webp'" icon="UserFilled" class="avatar"
+                         @click="goToPage('user', {id:authorUuid,menu:'article'})"></el-avatar>
               <el-row class="label" align="middle">{{ data.title }}</el-row>
             </el-row>
-            <el-button type="primary" icon="Plus">关注TA</el-button>
           </el-row>
           <el-row class="info" align="middle">
-            <span class="name">{{ data.name }}</span>
+            <span class="name">{{ user.username }}</span>
             <el-space>
-              <span class="word">{{ data.time }}</span>
+              <span class="word">{{ data.update }}</span>
               <span class="point"></span>
               <span class="word">{{
-                  (data.view > 1000 ? (data.view / 1000).toFixed(1) + "k" : data.view) + "阅读"
+                  (statistic.view > 1000 ? (statistic.view / 1000).toFixed(1) + "k" : statistic.view) + "阅读"
                 }}</span>
             </el-space>
           </el-row>
@@ -25,24 +28,39 @@
         <el-row class="main">
           <Editor
               class="editor"
-              v-model="valueHtml"
               :defaultConfig="editorConfig"
               :mode="mode"
               @onCreated="handleCreated"
           />
         </el-row>
-        <el-affix position="bottom" :offset="16" class="affix" @change="affixChange">
+        <el-affix position="bottom" :offset="0" class="affix" @change="affixChange">
           <el-row class="footer" justify="space-between">
             <el-space>
               <el-space :size="5" class="icon-block">
-                <el-icon class="iconfont icon-like" :size="20" color="var(--el-text-color-placeholder)"></el-icon>
+                <div ref="iconAgree" @click="agreeClick" :class="{'agree-select':statisticClicked['agree']}"
+                     class="icon agree-transform">
+                  <svg class="symbol-icon" aria-hidden="true" v-show="!statisticClicked['agree']">
+                    <use :xlink:href="'#icon-like'"></use>
+                  </svg>
+                  <svg class="symbol-icon" aria-hidden="true" v-show="statisticClicked['agree']">
+                    <use :xlink:href="'#icon-like-fill'"></use>
+                  </svg>
+                </div>
                 <span class="num">{{
-                    data.agree > 1000 ? (data.agree / 1000).toFixed(1) + "k" : data.agree
+                    statistic.agree > 1000 ? (statistic.agree / 1000).toFixed(1) + "k" : statistic.agree
                   }}</span>
               </el-space>
               <el-space :size="5" class="icon-block">
-                <el-icon class="iconfont icon-star" :size="20" color="var(--el-text-color-placeholder)"></el-icon>
-                <span class="num">收藏</span>
+                <div ref="iconCollect" @click="collectClick" :class="{'collect-select':statisticClicked['collect']}"
+                     class="icon collect-transform">
+                  <svg class="symbol-icon" aria-hidden="true" v-show="!statisticClicked['collect']">
+                    <use :xlink:href="'#icon-star'"></use>
+                  </svg>
+                  <svg class="symbol-icon" aria-hidden="true" v-show="statisticClicked['collect']">
+                    <use :xlink:href="'#icon-star-fill'"></use>
+                  </svg>
+                </div>
+                <span class="num">{{ statisticClicked['collect'] ? '已收藏' : '收藏' }}</span>
               </el-space>
             </el-space>
             <el-row class="reply" @click="comment">
@@ -74,43 +92,66 @@
     </el-row>
     <el-affix>
       <el-row class="talk-aside">
-        <Aside :data="data"></Aside>
+        <Aside :info="data" :statistic="statistic"></Aside>
       </el-row>
     </el-affix>
   </el-container>
 </template>
 
 <script setup>
-import {ref, onBeforeUnmount, shallowRef, onMounted} from "vue"
-import {Editor} from '@wangeditor/editor-for-vue'
 import Aside from "./component/aside.vue"
 import {goToPage} from "../../utils/globalFunc"
 import {scrollTo} from "../../utils/scroll";
+import {useRoute} from "vue-router";
+import {storeToRefs} from "pinia/dist/pinia.esm-browser";
+import {warning} from "../../utils/message";
+import {get, post} from "../../utils/axios";
+import {animationAgree, animationCollect} from "../../utils/animation";
+import {ref, onBeforeUnmount, shallowRef, onMounted} from "vue"
+import {Editor} from '@wangeditor/editor-for-vue'
+import {baseMainStore, userMainStore} from "../../store";
+import router from "../../router";
+import CollectionsChoose from "../collect/component/choose.vue";
 
 const editorRef = shallowRef()
-const valueHtml = ref('<p>hello</p>')
 const mode = ref('default')
 const editorConfig = {
   scroll: false,
   readOnly: true
 }
+const userStore = userMainStore()
+const baseStore = baseMainStore()
+const {uuid} = storeToRefs(userStore)
+const {avatar, talk} = storeToRefs(baseStore)
 
+let talkId = ref()
+let authorUuid = ref()
+let user = ref({})
 let affix = ref(false)
 let visible = ref(false)
+let iconAgree = ref(null)
+let iconCollect = ref(null)
+let loading = ref(false)
+let collectionsVisible = ref(false)
 let select = ref("hot")
+let agreeBounce = ref(1);
+let collectBounce = ref(1);
+let agreeAnimation = null
+let collectAnimation = null
+let clickLock = false
+
 
 let data = ref({
-  cover: "../../../../src/assets/images/cover.jpg",
-  image: "../../../../src/assets/images/boy.png",
-  title: "某科学的龙傲天dp分析法",
-  name: "刘小圆sama",
-  time: "发布于 2022-05-21",
-  view: 2000,
-  collect: 2000,
-  comment: 2000,
-  agree: 2000,
-  tags: ["Go", "kubernetes", "云原生", "Go", "kubernetes", "云原生"]
+  "title": "暂无数据",
+  "username": "暂无数据",
+  "update": "0000-00-00"
 })
+
+let statistic = ref({
+  "agree": 0,
+  "view": 0,
+})
+
 let options = ref([
   {
     label: "最热",
@@ -121,6 +162,189 @@ let options = ref([
     value: "new"
   }
 ])
+let statisticJudge = ref({
+  agree: false,
+  collect: false
+})
+let statisticClicked = ref({
+  agree: false,
+  collect: false
+})
+
+function init() {
+  animation()
+  initData()
+  getData()
+}
+
+function animation() {
+  agreeAnimation = animationAgree(iconAgree, agreeBounce)
+  collectAnimation = animationCollect(iconCollect, collectBounce)
+}
+
+function initData() {
+  let i = useRoute()
+  talkId.value = i.query["id"]
+  if (!talkId.value) {
+    talkNotExist()
+  }
+}
+
+function talkNotExist() {
+  router.push({name: "result", query: {type: "error", title: '获取讨论失败', description: "讨论不存在或已被删除"}})
+}
+
+function getData() {
+  getStatistic()
+}
+
+function getStatistic() {
+  loading.value = true
+  get("/v1/get/talk/statistic?id=" + talkId.value).then(function (reply) {
+    authorUuid.value = reply.data.uuid
+    statistic.value = reply.data
+    setView()
+    getUserInfo()
+  }).catch(function () {
+    talkNotExist()
+  })
+}
+
+function setView() {
+  post("/v1/set/talk/view", {
+    id: talkId.value,
+    uuid: authorUuid.value
+  })
+}
+
+function getUserInfo() {
+  get("/v1/get/user/info?uuid=" + authorUuid.value).then(function (reply) {
+    user.value = reply.data
+    getTalk()
+  }).catch(function () {
+    talkNotExist()
+  })
+}
+
+function getTalk() {
+  let url = talk.value.baseUrl + authorUuid.value + "/" + talkId.value + "/content"
+  get(url).then(function (reply) {
+    data.value = reply.data
+    editorRef.value.setHtml(data.value["html"])
+    getStatisticJudge()
+  }).catch(function () {
+    talkNotExist()
+  }).then(function () {
+    loading.value = false
+  })
+}
+
+function getStatisticJudge() {
+  if (!talkId.value || !uuid.value) {
+    return
+  }
+  post("/v1/talk/statistic/judge", {
+    id: talkId.value
+  }).then(function (reply) {
+    statisticJudge.value = reply.data
+    statisticClicked.value = reply.data
+  })
+}
+
+function agreeClick() {
+  if (!uuid.value) {
+    warning("账号未登录，请先登录")
+  }
+
+  if (!talkId.value || !authorUuid.value || clickLock) {
+    return
+  }
+
+  clickLock = true
+  if (!statisticJudge.value["agree"]) {
+    agreeAdd()
+  } else {
+    agreeCancel()
+  }
+}
+
+function agreeAdd() {
+  agreeAnimation.play()
+  statisticClicked.value["agree"] = true
+  statistic.value["agree"] += 1
+  post("/v1/set/talk/agree", {
+    id: talkId.value,
+    uuid: authorUuid.value,
+  }).then(function () {
+    statisticJudge.value["agree"] = true
+  }).catch(function () {
+    statisticClicked.value["agree"] = false
+    statistic.value["agree"] -= 1
+  }).then(function () {
+    clickLock = false
+  })
+}
+
+function agreeCancel() {
+  agreeAnimation.play()
+  statisticClicked.value["agree"] = false
+  statistic.value["agree"] -= 1
+  post("/v1/cancel/talk/agree", {
+    id: talkId.value,
+    uuid: authorUuid.value,
+  }).then(function () {
+    statisticJudge.value["agree"] = false
+  }).catch(function () {
+    statisticClicked.value["agree"] = true
+    statistic.value["agree"] += 1
+  }).then(function () {
+    clickLock = false
+  })
+}
+
+function collectClick() {
+  if (!uuid.value) {
+    warning("账号未登录，请先登录")
+  }
+
+  if (!talkId.value || !authorUuid.value) {
+    return
+  }
+
+  if (!statisticJudge.value['collect']) {
+    collectAdd()
+  } else {
+    collectCancel()
+  }
+}
+
+function collectAdd() {
+  collectionsVisible.value = true
+}
+
+function collectCancel() {
+  clickLock = true
+  collectAnimation.play()
+  statisticClicked.value["collect"] = false
+  statistic.value["collect"] -= 1
+  post("/v1/cancel/talk/collect", {
+    id: talkId.value,
+    uuid: authorUuid.value,
+  }).then(function () {
+    collectAnimation.play()
+  }).catch(function () {
+    statisticClicked.value["collect"] = true
+    statistic.value["collect"] += 1
+  }).then(function () {
+    clickLock = false
+  })
+}
+
+function collected() {
+  collectAnimation.play()
+  statisticClicked.value["collect"] = true
+  statistic.value["collect"] += 1
+}
 
 function handleCreated(editor) {
   editorRef.value = editor
@@ -139,6 +363,10 @@ function comment() {
   }
 }
 
+onMounted(function () {
+  init()
+})
+
 onBeforeUnmount(function () {
   const editor = editorRef.value
   if (editor == null) return
@@ -150,16 +378,27 @@ onBeforeUnmount(function () {
 .talk-container {
   margin: auto;
   width: fit-content;
+  padding: 20px 0;
+
+  ::v-deep(.collections-choose) {
+    border-radius: 4px;
+    margin-top: 10vh;
+
+    .el-dialog__body {
+      padding: 0 20px
+    }
+  }
 
   .talk-main {
     width: 734px;
 
     .talk-block {
-      padding: 16px;
+      width: 100%;
+      padding: 16px 16px 0 16px;
       margin-bottom: 10px;
       background-color: var(--el-color-white);
-      box-shadow: var(--el-box-shadow-lighter);
       border-radius: 8px;
+      border: 1px solid var(--el-border-color-lighter);
 
       .header {
         width: 100%;
@@ -171,7 +410,7 @@ onBeforeUnmount(function () {
             width: calc(100% - 120px);
 
             .avatar {
-              margin-right: 15px;
+              margin-right: 10px;
               cursor: pointer;
             }
 
@@ -223,12 +462,36 @@ onBeforeUnmount(function () {
 
         .footer {
           width: 100%;
-          padding-top: 16px;
+          padding: 16px 0;
           border-top: 1px solid var(--el-border-color);
           background-color: var(--el-color-white);
 
           .icon-block {
             cursor: pointer;
+
+            .agree-select {
+              color: var(--el-color-primary) !important;
+            }
+
+            .collect-select {
+              color: #ffa500 !important;
+            }
+
+            .icon {
+              display: inline-flex;
+              position: relative;
+              height: 20px;
+              font-size: 20px;
+              color: var(--el-text-color-placeholder);
+            }
+
+            .agree-transform {
+              transform: scale3d(v-bind(agreeBounce), v-bind(agreeBounce), 1);
+            }
+
+            .collect-transform {
+              transform: scale3d(v-bind(collectBounce), v-bind(collectBounce), 1)
+            }
 
             .num {
               color: var(--el-text-color-secondary);
@@ -258,7 +521,7 @@ onBeforeUnmount(function () {
     .statistic-block {
       width: 100%;
       border-radius: 8px;
-      box-shadow: var(--el-box-shadow-lighter);
+      border: 1px solid var(--el-border-color-lighter);
       background-color: var(--el-color-white);
       margin-bottom: 10px;
       padding: 10px 15px;
