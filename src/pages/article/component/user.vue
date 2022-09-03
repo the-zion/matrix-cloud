0e8fe33afa287d6dbbc1718fda9796d4d2b95fda
liusiyuan <945212191@qq.com>
2022-09-03 18:15:37 +0800

    refactor: update page user of article

--- src/pages/article/component/user.vue
+++ src/pages/article/component/user.vue
@@ -3,7 +3,6 @@
     <el-empty v-show="data.length === 0 && !loading" class="empty" description=" "
               :image-size="250" image="../../src/assets/images/no_data.svg"
     />
-    <el-skeleton class="skeleton" v-show="loading" :rows="2" animated/>
     <el-row class="data" fill :size="0">
       <el-row v-for="item in data" class="each" :key="item.id"
               @click="goToPage('article', {id:item.id})">
@@ -58,39 +57,9 @@
             </el-space>
           </el-space>
         </el-row>
-        <el-space class="operation" v-if="userId === uuid" :style="{'display':item['opShow'] || 'none'}">
-          <el-space :size="3" @click.stop="articleEdit(item)" class="op">
-            <el-icon>
-              <EditPen/>
-            </el-icon>
-            <span class="word">编辑</span>
-          </el-space>
-          <el-popconfirm title="文章删除确认"
-                         confirm-button-text="确定"
-                         cancel-button-text="取消"
-                         @confirm="articleDelete(item)"
-                         @cancel="opCancel(item)"
-          >
-            <template #reference>
-              <el-space :size="3" @click.stop="item['opShow'] = 'inline-flex'" class="op">
-                <el-icon>
-                  <Delete/>
-                </el-icon>
-                <span class="word">删除</span>
-              </el-space>
-            </template>
-          </el-popconfirm>
-        </el-space>
       </el-row>
     </el-row>
-    <el-row class="foot" justify="center" v-if="data.length !== 0 || currentPage > 1">
-      <el-pagination
-          v-model:current-page="currentPage"
-          :page-size="10"
-          layout="prev, pager, next"
-          :total="pageTotal"
-      />
-    </el-row>
+    <el-skeleton class="skeleton" v-show="loading" :rows="2" animated/>
   </el-row>
 </template>
 
@@ -101,15 +70,13 @@ export default {
 </script>
 
 <script setup>
-import {onMounted, ref, watch} from "vue"
+import {onBeforeMount, ref} from "vue"
 import {goToPage} from "../../../utils/globalFunc";
 import {baseMainStore, userMainStore} from "../../../store";
 import {storeToRefs} from "pinia/dist/pinia.esm-browser";
-import {axiosGetAll, get, post} from "../../../utils/axios";
-import {error, success} from "../../../utils/message";
+import {axiosGetAll, get} from "../../../utils/axios";
 import {useRoute} from "vue-router";
-import {scrollTo} from "../../../utils/scroll";
-import router from "../../../router";
+import {scrollToBottomListen, throttle} from "../../../utils/scroll";
 
 const userStore = userMainStore()
 const baseStore = baseMainStore()
@@ -119,81 +86,47 @@ const {uuid} = storeToRefs(userStore)
 let data = ref([])
 let list = ref([])
 let loading = ref(false)
-let currentPage = ref(1)
-let pageTotal = ref(1)
+let isBottom = ref(false)
+let currentPage = 1
 let userId = ref()
 let request = null
+let getDataLock = false
 
 function init() {
   initData()
   getData()
-  getDataCount()
 }
 
 function initData() {
   userId.value = useRoute().query["id"]
+  scrollToBottomListen(throttle(scrollToBottom, 1000))
+}
+
+function scrollToBottom() {
+  getData()
 }
 
 function getData() {
-  if (!userId.value) {
+  if (!userId.value || isBottom.value || getDataLock) {
     return
   }
-  data.value = []
   list.value = []
   loading.value = true
-  if (userId.value === uuid.value) {
-    getUserArticle()
-  } else {
-    getUserArticleVisitor()
-  }
-}
-
-function getDataCount() {
-  if (!userId.value) {
-    return
-  }
-  if (userId.value === uuid.value) {
-    getUserArticleCount()
-  } else {
-    getUserArticleVisitorCount()
-  }
-}
-
-function getUserArticle() {
-  post("/v1/get/user/article/list", {page: currentPage.value}).then(function (reply) {
+  getDataLock = true
+  get("/v1/get/user/article/list/visitor?page=" + currentPage + "&uuid=" + userId.value).then(function (reply) {
     list.value = reply.data.article
-    request = 2
-    getStatistic()
-    getIntroduce()
-  }).catch(function (){
-    loading.value = false
-  })
-}
-
-function getUserArticleCount() {
-  post("/v1/get/article/count", {}).then(function (reply) {
-    pageTotal.value = reply.data.count
-  })
-}
-
-function getStatistic() {
-  let ids = []
-  list.value.forEach(function (each) {
-    ids.push("ids=" + each["id"])
-  })
-  get("/v1/get/article/list/statistic?" + ids.join("&")).then(function (reply) {
-    reply.data.count.forEach(function (each) {
-      list.value.forEach(function (item, index) {
-        each.id === item["id"] && (list.value[index] = Object.assign(item, each))
-      })
-    })
-  }).catch(function () {
-  }).then(function () {
-    request -= 1
-    if (request === 0) {
-      data.value = list.value
+    let size = reply.data.article.length
+    if (size === 0) {
+      isBottom.value = true
       loading.value = false
+      getDataLock = false
+      return
     }
+    currentPage += 1
+    getIntroduce()
+  }).catch(function () {
+    loading.value = false
+    getDataLock = false
   })
 }
 
@@ -210,59 +143,13 @@ function getIntroduce() {
     })
   }, function () {
   }, function () {
-    request -= 1
-    if (request === 0) {
-      data.value = list.value
-      loading.value = false
-    }
-  })
-}
-
-function getUserArticleVisitorCount() {
-  get("/v1/get/article/count/visitor?uuid=" + userId.value).then(function (reply) {
-    pageTotal.value = reply.data.count
-  })
-}
-
-function getUserArticleVisitor() {
-  get("/v1/get/user/article/list/visitor?page=" + currentPage.value + "&uuid=" + userId.value).then(function (reply) {
-    list.value = reply.data.article
-    request = 2
-    getStatistic()
-    getIntroduce()
-  }).catch(function (){
+    data.value = data.value.concat(list.value)
     loading.value = false
+    getDataLock = false
   })
 }
 
-function articleEdit(item) {
-  const {href} = router.resolve({name: "article.write", query: {mode: 'edit', id: item.id}});
-  window.open(href, "_blank");
-}
-
-function articleDelete(item) {
-  post("/v1/delete/article", {
-    id: item.id,
-  }).then(function () {
-    success("删除成功")
-    getData()
-  }).catch(function () {
-    error("删除失败")
-  })
-}
-
-function opCancel(item) {
-  setTimeout(function () {
-    item['opShow'] = 'none'
-  }, 400)
-}
-
-watch(currentPage, () => {
-  scrollTo("user-list")
-  getData()
-})
-
-onMounted(function () {
+onBeforeMount(function () {
   init()
 })
 </script>
@@ -372,41 +259,16 @@ onMounted(function () {
           }
         }
       }
-
-      .operation {
-        position: absolute;
-        display: none;
-        top: 16px;
-        right: 0;
-        color: var(--el-text-color-placeholder);
-        cursor: pointer;
-
-        .op {
-          .icon {
-            font-size: 20px;
-          }
-
-          .word {
-            font-size: 14px;
-          }
-        }
-
-        .op:hover {
-          color: var(--el-color-primary);
-        }
-      }
-    }
-
-    .each:hover {
-      .operation {
-        display: inline-flex !important;
-      }
     }
   }
 
   .foot {
     margin-top: 1rem;
     width: 100%;
+  }
+
+  .skeleton {
+    padding: 16px;
   }
 }
 </style>
