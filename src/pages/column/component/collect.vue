3d63555f6396d53d42bfd20f50ecd7b0ddc9fc32
liusiyuan <945212191@qq.com>
2022-09-04 23:53:48 +0800

    refactor: update page eollect of column

--- src/pages/column/component/collect.vue
+++ src/pages/column/component/collect.vue
@@ -3,9 +3,8 @@
     <el-empty v-show="data.length === 0 && !loading" class="empty" description=" "
               :image-size="250" image="../../src/assets/images/no_data.svg"
     />
-    <el-skeleton class="skeleton" v-show="loading" :rows="2" animated/>
     <el-space class="data" fill :size="0">
-      <el-row v-for="item in data" class="each" :key="item.id"
+      <el-row v-for="(item, index) in data" class="each" :key="item.id"
               @click="goToPage('column', {id:item.id})">
         <el-row class="column-card" align="top">
           <el-image class="image" fit="cover" :src="item.cover" lazy></el-image>
@@ -40,33 +39,16 @@
                     item.collect > 1000 ? (item.collect / 1000).toFixed(1) + "k" : item.collect
                   }}</span>
               </el-space>
-            </el-space>
-          </el-row>
-        </el-row>
-        <el-space class="operation" v-if="props.userId === uuid">
-          <el-popconfirm title="取消收藏确认"
-                         confirm-button-text="确定"
-                         cancel-button-text="取消"
-                         @confirm="cancelCollect(item)"
-          >
-            <template #reference>
-              <el-space :size="3" @click.stop="">
+              <el-space :size="3" @click.stop="collectDeleteConfirm(item, index)" v-if="props.userId === uuid">
                 <el-icon class="iconfont icon-star icon"></el-icon>
                 <span class="word">取消收藏</span>
               </el-space>
-            </template>
-          </el-popconfirm>
-        </el-space>
+            </el-space>
+          </el-row>
+        </el-row>
       </el-row>
     </el-space>
-    <el-row class="foot" justify="center" v-if="data.length !== 0 || currentPage > 1">
-      <el-pagination
-          v-model:current-page="currentPage"
-          :page-size="10"
-          layout="prev, pager, next"
-          :total="pageTotal"
-      />
-    </el-row>
+    <el-skeleton class="skeleton" v-show="loading" :rows="2" animated/>
   </el-row>
 </template>
 
@@ -78,12 +60,13 @@ export default {
 
 <script setup>
 import {baseMainStore, userMainStore} from "../../../store";
-import {goToPage} from "../../../utils/globalFunc";
+import {confirm, goToPage} from "../../../utils/globalFunc";
 import {storeToRefs} from "pinia/dist/pinia.esm-browser";
-import {onMounted, ref} from "vue";
+import {onBeforeMount, ref} from "vue";
 import {useRoute} from "vue-router";
 import {axiosGetAll, get, post} from "../../../utils/axios";
 import {error, success} from "../../../utils/message";
+import {scrollToBottomListen, throttle} from "../../../utils/scroll";
 
 const userStore = userMainStore()
 const baseStore = baseMainStore()
@@ -92,61 +75,51 @@ const {uuid} = storeToRefs(userStore)
 const props = defineProps({
   userId: String
 })
+const emits = defineEmits(["row-delete"])
 
 let data = ref([])
 let list = ref([])
 let loading = ref(false)
-let currentPage = ref(1)
-let pageTotal = ref(1)
+let currentPage = 1
 let collectionsId = null
 let request = 0
+let isBottom = false
+let getDataLock = false
 
 function init() {
   initData()
   getData()
-  getDataCount()
 }
 
 function initData() {
   collectionsId = useRoute().query["id"]
+  scrollToBottomListen(throttle(scrollToBottom, 1000))
 }
 
+function scrollToBottom() {
+  getData()
+}
 
 function getData() {
-  if (!collectionsId) {
+  if (!collectionsId || isBottom || getDataLock) {
     return
   }
-  data.value = []
-  list.value = []
   loading.value = true
-  get("/v1/get/collect/column?id=" + collectionsId + "&page=" + currentPage.value).then(function (reply) {
+  getDataLock = true
+  get("/v1/get/collect/column/list?id=" + collectionsId + "&page=" + currentPage).then(function (reply) {
     list.value = reply.data.column
-    request = 2
-    getStatistic()
-    getIntroduce()
-  }).catch(function (){
-    loading.value = false
-  })
-}
-
-function getStatistic() {
-  let ids = []
-  list.value.forEach(function (each) {
-    ids.push("ids=" + each["id"])
-  })
-  get("/v1/get/column/list/statistic?" + ids.join("&")).then(function (reply) {
-    reply.data.count.forEach(function (each) {
-      list.value.forEach(function (item, index) {
-        each.id === item["id"] && (list.value[index] = Object.assign(item, each))
-      })
-    })
-  }).catch(function () {
-  }).then(function () {
-    request -= 1
-    if (request === 0) {
-      data.value = list.value
+    let size = reply.data.column.length
+    if (size === 0) {
+      isBottom = true
       loading.value = false
+      getDataLock = false
+      return
     }
+    currentPage += 1
+    getIntroduce()
+  }).catch(function () {
+    loading.value = false
+    getDataLock = false
   })
 }
 
@@ -163,37 +136,33 @@ function getIntroduce() {
     })
   }, function () {
   }, function () {
-    request -= 1
-    if (request === 0) {
-      data.value = list.value
-      loading.value = false
-    }
+    data.value = data.value.concat(list.value)
+    loading.value = false
+    getDataLock = false
   })
 }
 
-function getDataCount() {
-  if (!collectionsId) {
-    return
-  }
-  get("/v1/get/collect/column/count?id=" + collectionsId).then(function (reply) {
-    pageTotal.value = reply.data.count
+function collectDeleteConfirm(item, index) {
+  confirm("收藏取消", "确认取消该收藏吗").then(function () {
+    cancelCollect(item, index)
   })
 }
 
-function cancelCollect(item) {
+function cancelCollect(item, index) {
   post("/v1/cancel/column/collect", {
     id: item.id,
     uuid: item.uuid,
   }).then(function () {
     success("收藏已取消")
-    getData()
+    data.value.splice(index, 1);
+    emits("row-delete")
   }).catch(function () {
     error("取消收藏失败")
   })
 }
 
 
-onMounted(function () {
+onBeforeMount(function () {
   init()
 })
 </script>
@@ -278,38 +247,13 @@ onMounted(function () {
             color: var(--el-text-color-secondary)
           }
 
-          .num {
+          .num, .word {
             font-size: 14px;
             color: var(--el-text-color-secondary)
           }
         }
       }
-
-      .operation {
-        position: absolute;
-        bottom: 16px;
-        right: 16px;
-        color: var(--el-text-color-placeholder);
-        cursor: pointer;
-
-        .icon {
-          font-size: 20px;
-        }
-
-        .word {
-          font-size: 14px;
-        }
-      }
-
-      .operation:hover {
-        color: var(--el-color-primary);
-      }
     }
-  }
-
-  .foot {
-    margin-top: 1rem;
-    width: 100%;
   }
 }
 </style>
