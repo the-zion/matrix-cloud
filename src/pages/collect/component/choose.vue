83f678aeb570d2fbcc3e8a1a6e872b0aed30c935
liusiyuan <945212191@qq.com>
2022-09-04 23:51:49 +0800

    refactor: update page choose of collect

--- src/pages/collect/component/choose.vue
+++ src/pages/collect/component/choose.vue
@@ -4,10 +4,15 @@
              @closed="closed"
   >
     <el-row class="table">
-      <el-table :data="gridData" highlight-current-row @current-change="change" :row-style="{'cursor':'pointer'}"
+      <el-table :data="filterTableData" highlight-current-row @current-change="change" :row-style="{'cursor':'pointer'}"
                 v-show="!loading">
         <el-table-column type="index" label="序号" width="60" :index="indexFormatter"/>
         <el-table-column show-overflow-tooltip class="column" property="name" label="我的收藏集" min-width="1"/>
+        <el-table-column align="right" min-width="1">
+          <template #header>
+            <el-input v-model="search" size="small" placeholder="搜索名称"/>
+          </template>
+        </el-table-column>
         <template #empty>
           <el-row align="middle">
             <span>暂无收藏集，马上去创建一个吧</span>
@@ -16,15 +21,6 @@
         </template>
       </el-table>
       <el-skeleton class="skeleton" v-show="loading" :rows="2" animated/>
-    </el-row>
-    <el-row class="footer" justify="center" v-if="gridData.length">
-      <el-pagination
-          class="page"
-          v-model:current-page="currentPage"
-          :page-size="10"
-          layout="prev, pager, next"
-          :total="pageTotal"
-      />
     </el-row>
     <template #footer>
       <span class="foot">
@@ -44,14 +40,14 @@ export default {
 </script>
 
 <script setup>
-import {ref, watch} from "vue"
-import {post} from "../../../utils/axios";
+import {ref, computed} from "vue"
+import {post, get, axiosGetAll} from "../../../utils/axios";
 import {error} from "../../../utils/message";
 import router from "../../../router";
-import {userMainStore} from "../../../store";
+import {baseMainStore, userMainStore} from "../../../store";
 import {storeToRefs} from "pinia/dist/pinia";
 
-const emits = defineEmits(["update:visible", "update:judge", "collected"])
+const emits = defineEmits(["update:visible", "collected"])
 const props = defineProps({
   visible: Boolean,
   judge: Object,
@@ -60,16 +56,27 @@ const props = defineProps({
   uuid: String,
 })
 const userStore = userMainStore()
+const baseStore = baseMainStore()
 const {uuid} = storeToRefs(userStore)
+const {avatar, collections} = storeToRefs(baseStore)
+const filterTableData = computed(() =>
+    gridData.value.filter(
+        (data) =>
+            !search.value ||
+            data.name.toLowerCase().includes(search.value.toLowerCase())
+    )
+)
+
 
 let judge = null
 let mode = null
 let currentPage = ref(1)
-let pageTotal = ref(1)
 let gridData = ref([])
+let list = ref([])
 let loading = ref(false)
 let collectLoading = ref(false)
 let selectId = ref()
+let search = ref('')
 let collectUrl = {
   article: "/v1/set/article/collect",
   talk: "/v1/set/talk/collect",
@@ -83,13 +90,11 @@ function open() {
 function init() {
   initData()
   getData()
-  getDataCount()
 }
 
 function initData() {
   gridData.value = []
   currentPage.value = 1
-  pageTotal.value = 1
   selectId.value = 0
   loading.value = false
   collectLoading.value = false
@@ -99,20 +104,30 @@ function initData() {
 
 function getData() {
   loading.value = true
-  post("/v1/get/collections", {
-    page: currentPage.value
-  }).then(function (reply) {
-    gridData.value = reply.data.collections
-    loading.value = false
+  get("/v1/get/collections/list/all").then(function (reply) {
+    list.value = reply.data.collections
+    getContent()
   }).catch(function () {
     loading.value = false
     error("获取收藏集失败")
   })
 }
 
-function getDataCount() {
-  post("/v1/get/collections/count", {}).then(function (reply) {
-    pageTotal.value = reply.data.count
+function getContent() {
+  let endpoints = []
+  list.value.forEach(function (item) {
+    endpoints.push(collections.value.baseUrl + uuid.value + "/" + item["id"] + "/content")
+  })
+  axiosGetAll(endpoints, function (allData) {
+    allData.forEach(function (each) {
+      list.value.forEach(function (item, index) {
+        each.data.id === item["id"] && (list.value[index] = Object.assign(item, each.data))
+      })
+    })
+  }, function () {
+  }, function () {
+    gridData.value = list.value
+    loading.value = false
   })
 }
 
@@ -134,8 +149,6 @@ function collect() {
     collections_id: selectId.value,
     uuid: props.uuid
   }).then(function () {
-    judge["collect"] = true
-    emits("judge", judge)
     emits("collected")
     cancel()
   }).catch(function () {
@@ -151,8 +164,8 @@ function closed() {
 
 function create() {
   const {href} = router.resolve({
-    name: "user",
-    query: {id: uuid.value, menu: 'collect'}
+    name: "creation.content",
+    query: {menu: 'collect'}
   });
   window.open(href, "_blank");
 }
@@ -160,10 +173,6 @@ function create() {
 function indexFormatter(index) {
   return (index + 1) + (currentPage.value - 1) * 10
 }
-
-watch(currentPage, () => {
-  getData()
-})
 </script>
 
 <style scoped lang="scss">
