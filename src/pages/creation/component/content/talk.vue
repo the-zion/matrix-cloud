5430b5f86a329649b3ce1f75717e87d622bac2fc
liusiyuan <945212191@qq.com>
2022-09-05 18:30:56 +0800

    feat: add page talk of creation

---
+++ src/pages/creation/component/content/talk.vue
@@ -0,0 +1,54 @@
+<template>
+  <el-container class="talk-container">
+    <el-row class="bar" justify="space-between" align="middle">
+      <span>讨论</span>
+    </el-row>
+    <el-row class="body">
+      <talk-creation-list @row-delete="rowDelete"></talk-creation-list>
+    </el-row>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: "Talk"
+}
+</script>
+
+<script setup>
+import TalkCreationList from "../../../talk/component/creation.vue";
+
+const emits = defineEmits(["row-delete"])
+
+function rowDelete() {
+  emits("row-delete")
+}
+</script>
+
+<style scoped lang="scss">
+.talk-container {
+  width: 100%;
+  flex-direction: column;
+
+  .bar {
+    width: 100%;
+    height: 50px;
+    padding: 0 20px;
+    border-bottom: 1px solid var(--el-border-color-light);
+
+    .operation {
+      font-size: 14px;
+      color: var(--el-text-color-secondary);
+      cursor: pointer;
+
+      .select {
+        color: var(--el-color-primary);
+      }
+    }
+  }
+
+  .body {
+    width: 100%;
+  }
+}
+</style>
