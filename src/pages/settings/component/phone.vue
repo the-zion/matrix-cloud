4075248f68fa6064cd1fafcfc0573e153a174157
liusiyuan <945212191@qq.com>
2022-09-06 18:36:47 +0800

    refactor: update onMounted to onBeforeMount

--- src/pages/settings/component/phone.vue
+++ src/pages/settings/component/phone.vue
@@ -23,7 +23,7 @@ export default {
 </script>
 
 <script setup>
-import {ref, onMounted} from "vue";
+import {ref, onBeforeMount} from "vue";
 
 import {validatePhone, validateCode} from "../../../utils/check";
 import {post} from "../../../utils/axios";
@@ -67,7 +67,7 @@ function countDown() {
   }, 1000)
 }
 
-onMounted(function () {
+onBeforeMount(function () {
   emits("open", form.value, formRef.value)
 })
 </script>
