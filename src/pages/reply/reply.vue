a203b4df54b8db873d5ebc2034bc00fb9003a22b
liusiyuan <945212191@qq.com>
2022-09-06 18:31:48 +0800

    feat: add page about reply

--- src/pages/reply/reply.vue
+++ src/pages/reply/reply.vue
@@ -5,7 +5,7 @@
           class="editor"
           v-model="valueHtml"
           :defaultConfig="editorConfig"
-          :mode="mode"
+          :mode="'default'"
           @onCreated="handleCreated"
       />
     </el-row>
@@ -14,9 +14,9 @@
           class="toolbar"
           :editor="editorRef"
           :defaultConfig="toolbarConfig"
-          :mode="mode"
+          :mode="'default'"
       />
-      <el-button class="button" type="primary" size="small">评论</el-button>
+      <el-button class="button" type="primary" size="small" :loading="sending" @click="commit">评论</el-button>
     </el-row>
   </el-row>
 </template>
@@ -28,12 +28,16 @@ export default {
 </script>
 
 <script setup>
-import {onBeforeUnmount, ref, shallowRef, onMounted} from 'vue'
+import {onBeforeUnmount, ref, shallowRef, onBeforeMount} from 'vue'
 import {Editor, Toolbar} from '@wangeditor/editor-for-vue'
+import {get, post} from "../../utils/axios";
+import {success, error, warning} from "../../utils/message";
+import {initCos} from "../../utils/cos";
+import {baseMainStore, userMainStore} from "../../store";
+import {storeToRefs} from "pinia/dist/pinia.esm-browser";
 
 const editorRef = shallowRef()
 const valueHtml = ref('')
-const mode = ref('default')
 const toolbarConfig = {
   excludeKeys: [
     "headerSelect",
@@ -65,9 +69,156 @@ const editorConfig = {
   scroll: true,
   maxLength: 1000,
 }
+const props = defineProps({
+  creationId: Number,
+  creationType: Number,
+  mode: {
+    type: String,
+    default: "comment"
+  },
+  rootId: Number,
+  parentId: {
+    type: Number,
+    default: 0
+  }
+})
+const cos = initCos()
+const userStore = userMainStore()
+const baseStore = baseMainStore()
+const {uuid} = storeToRefs(userStore)
+const {comment} = storeToRefs(baseStore)
+
+let draftId = ref()
+let sending = ref(false)
+let creationId = null
+let creationType = null
+let rootId = null
+let parentId = null
+let mode = null
+let token = null
+let commentParams = {}
+let uploadParams = {
+  Bucket: comment.value.bucket,
+  Region: comment.value.region,
+}
 
 function handleCreated(editor) {
   editorRef.value = editor
+}
+
+function init() {
+  initData()
+  getLastDraft()
+}
+
+function initData() {
+  creationId = props.creationId
+  creationType = props.creationType
+  rootId = props.rootId
+  parentId = props.parentId
+  mode = props.mode
+  token = localStorage.getItem("matrix-token")
+}
+
+function getLastDraft() {
+  get("/v1/get/last/comment/draft").then(function (reply) {
+    if (reply.data.status !== 1) {
+      CreateDraft()
+    } else {
+      draftId.value = reply.data.id
+    }
+  }).catch(function (err) {
+    let response = err.response
+    if (response) {
+      switch (response.data.reason) {
+        case "RECORD_NOT_FOUND":
+          CreateDraft()
+          return
+      }
+    }
+  })
+}
+
+function CreateDraft() {
+  post("/v1/create/comment/draft", {}).then(function (reply) {
+    draftId.value = reply.data.id
+  })
+}
+
+function commit() {
+  if (!uuid.value && !token) {
+    warning("账号未登录，请先登录")
+    return
+  }
+
+  if (!draftId.value) {
+    error("评论发布失败")
+    return
+  }
+
+  if (mode === 'comment' && (!creationId || !creationType)) {
+    error("评论发布失败")
+    return
+  }
+
+  if (mode === 'sub_comment' && (!rootId)) {
+    error("评论发布失败")
+    return
+  }
+
+  if (editorRef.value.getText().length === 0) {
+    error("评论内容不能为空白")
+    return
+  }
+
+  setCommentParams()
+  commitContent()
+}
+
+function setCommentParams() {
+  commentParams["html"] = editorRef.value.getHtml()
+  commentParams["update"] = new Date().toLocaleDateString()
+  commentParams["id"] = draftId.value
+}
+
+function commitContent() {
+  sending.value = true
+  uploadParams["Key"] = comment.value.key + uuid.value + "/" + draftId.value + "/content"
+  uploadParams["Headers"] = {
+    'x-cos-meta-token': token,
+    'x-cos-meta-id': draftId.value + ""
+  }
+  if (mode === 'comment') {
+    uploadParams["Headers"]['x-cos-meta-creationId'] = creationId
+    uploadParams["Headers"]['x-cos-meta-creationType'] = creationType
+  }
+
+  if (mode === 'sub_comment') {
+    uploadParams["Headers"]['x-cos-meta-rootId'] = rootId
+    uploadParams["Headers"]['x-cos-meta-parentId'] = parentId
+  }
+
+  uploadParams["Body"] = JSON.stringify(commentParams)
+  cos.uploadFile(uploadParams, function (err) {
+    if (err) {
+      error("评论发布失败")
+      return
+    }
+    sendComment()
+  })
+}
+
+function sendComment() {
+  post(mode === "comment" ? "/v1/send/comment" : "/v1/send/subcomment", {id: draftId.value}).then(function () {
+    success('评论已提交审核，审核通过即发布到评论区中')
+    draftId.value = null
+    getLastDraft()
+    editorRef.value.clear()
+  }).catch(function (err) {
+    error("评论发布失败")
+  }).then(function () {
+    sending.value = false
+  })
 }
 
 onBeforeUnmount(() => {
@@ -75,6 +226,11 @@ onBeforeUnmount(() => {
   if (editor == null) return
   editor.destroy()
 })
+
+onBeforeMount(function () {
+  init()
+})
+
 </script>
 
 <style scoped lang="scss">
