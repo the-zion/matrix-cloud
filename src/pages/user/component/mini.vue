7ca060917c789239591023fa5e89b27faf0b8c2b
liusiyuan <945212191@qq.com>
2022-09-14 18:40:37 +0800

    refactor: update page mini about user

--- src/pages/user/component/mini.vue
+++ src/pages/user/component/mini.vue
@@ -1,12 +1,14 @@
 <template>
-  <el-row class="card">
+  <el-row class="card" v-loading="loading">
     <el-row class="head">
-      <el-avatar @click="goToPage('user', 1)" class="avatar" shape="square" :size="60" :src="image"/>
+      <el-avatar @click="goToPage('user', {id:userId,menu:'article'})" icon="UserFilled" class="avatar" shape="square"
+                 :size="60" :src="avatar.baseUrl + userId + '/avatar.webp'"/>
       <el-space class="info" direction="vertical" alignment="start" :size="0" fill>
         <el-space>
-          <span class="nickname">{{ nickname }}</span>
+          <span class="nickname">{{ data.username }}</span>
+          <el-icon class="level iconfont" :class="level"></el-icon>
         </el-space>
-        <span class="introduce">{{ introduce }}</span>
+        <span class="introduce">{{ data.introduce || "此人很懒，没有任何简介～" }}</span>
       </el-space>
     </el-row>
     <el-row class="achievement">
@@ -15,7 +17,7 @@
           <span class="label">{{ item.label }}</span>
           <span
               class="value">{{
-              achievement[item.key] > 1000 ? (achievement[item.key] / 1000).toFixed(1) + "k" : achievement[item.key]
+              data[item.key] > 1000 ? (data[item.key] / 1000).toFixed(1) + "k" : data[item.key] || 0
             }}</span>
         </el-space>
         <el-divider class="divider" v-show="index !== achMeta.length - 1" direction="vertical"/>
@@ -23,7 +25,11 @@
     </el-row>
     <el-row class="foot" justify="center">
       <el-button class="button" icon="Message" type="info" bg text>留言</el-button>
-      <el-button class="button" icon="Plus" type="primary">关注</el-button>
+      <el-button class="button" :type="follows[userId]?'info':'primary'" :icon="follows[userId]?'':'Plus'"
+                 :loading="followLoading"
+                 @click="follow()" :text="follows[userId]" :bg="follows[userId]">
+        {{ follows[userId] ? '取消关注' : '关注' }}
+      </el-button>
     </el-row>
   </el-row>
 </template>
@@ -35,12 +41,21 @@ export default {
 </script>
 
 <script setup>
-import {ref} from "vue"
 import {goToPage} from "../../../utils/globalFunc";
+import {storeToRefs} from "pinia/dist/pinia";
+import {get, post} from "../../../utils/axios";
+import {onBeforeMount, ref} from "vue"
+import {baseMainStore, userMainStore} from "../../../store";
+import {error, success, warning} from "../../../utils/message";
 
-let nickname = ref("刘小圆sama")
-let introduce = ref("本文的目的是为了总结出一个通用的，改动较少的非递归模板，可以分别适用于二叉树的前序、中序和后续遍历，方便记忆。前序遍历前序遍历的遍历顺序为：根 -> 左 -> 右。只要有左子树，就把左子树入栈，同时把值加入答案数组，然后依次弹出栈顶元素，移动到它的右子树，重复操作。「代码块」中序遍历中序遍历的遍")
-let image = ref("../../src/assets/images/boy.png")
+const props = defineProps({
+  uuid: String
+})
+const userStore = userMainStore()
+const baseStore = baseMainStore()
+const {uuid} = storeToRefs(userStore)
+const {avatar} = storeToRefs(baseStore)
+
 let achMeta = ref([
   {
     key: "view",
@@ -59,12 +74,122 @@ let achMeta = ref([
     label: "关注者"
   }
 ])
+let userId = ref()
+let data = ref({})
+let achievement = ref({})
+let loading = ref(true)
+let followLoading = ref(false)
+let follows = ref({})
+let level = ref("icon-level1")
+let levelColor = ref("#bdc1c6")
 
-let achievement = ref({
-  view: 10200,
-  agree: 9,
-  collect: 13,
-  followed: 0
+function init() {
+  initData()
+  getUserInfo()
+  getUserFollows()
+}
+
+function initData() {
+  userId.value = props.uuid
+}
+
+let levelMap = {
+  "level1": {"icon": "icon-level1", "color": "#bdc1c6"},
+  "level2": {"icon": "icon-level2", "color": "#00e676"},
+  "level3": {"icon": "icon-level3", "color": "#40c4ff"},
+  "level4": {"icon": "icon-level4", "color": "#ffab40"},
+  "level5": {"icon": "icon-level5", "color": "#ff6500"},
+  "level6": {"icon": "icon-level6", "color": "#ff1744"},
+}
+
+function getUserInfo() {
+  if (!userId.value) {
+    return
+  }
+  get("/v1/get/user/info/visitor?uuid=" + userId.value).then(function (reply) {
+    data.value = reply.data
+    loading.value = false
+    getLevel()
+  })
+}
+
+function getLevel() {
+  let score = data.value["score"]
+  let currentLevel
+  if (score >= 0 && score < 20) {
+    currentLevel = "level1"
+  } else if (score >= 20 && score < 150) {
+    currentLevel = "level2"
+  } else if (score >= 150 && score < 450) {
+    currentLevel = "level3"
+  } else if (score >= 450 && score < 1080) {
+    currentLevel = "level4"
+  } else if (score >= 1080 && score < 2880) {
+    currentLevel = "level5"
+  } else {
+    currentLevel = "level6"
+  }
+  level.value = levelMap[currentLevel]["icon"]
+  levelColor.value = levelMap[currentLevel]["color"]
+}
+
+function getUserFollows() {
+  if (!userId.value) {
+    return
+  }
+  get("/v1/get/user/follows").then(function (reply) {
+    follows.value = reply.data["follows"]
+  })
+}
+
+function follow() {
+  if (!userId.value) {
+    return
+  }
+
+  if (!uuid.value) {
+    warning("账号未登录，请先登录")
+    return
+  }
+
+  if (userId.value === uuid.value) {
+    warning("不要自己关注自己哦～")
+    return
+  }
+
+  if (follows.value[userId.value]) {
+    cancelFollow()
+  } else {
+    setFollow()
+  }
+}
+
+function setFollow() {
+  followLoading.value = true
+  post("/v1/set/user/follow", {uuid: userId.value}).then(function () {
+    success("已关注")
+    follows.value[userId.value] = true
+  }).catch(function () {
+    error("关注出错，请稍后再试")
+  }).then(function () {
+    followLoading.value = false
+  })
+}
+
+function cancelFollow() {
+  followLoading.value = true
+  post("/v1/cancel/user/follow", {uuid: userId.value}).then(function () {
+    success("已取消关注")
+    follows.value[userId.value] = false
+  }).catch(function () {
+    error("取消出错，请稍后再试")
+  }).then(function () {
+    followLoading.value = false
+  })
+}
+
+onBeforeMount(function () {
+  init()
 })
 
 </script>
@@ -82,6 +207,7 @@ let achievement = ref({
       margin-right: 12px;
       border-radius: 6px;
       cursor: pointer;
+      font-size: 35px;
     }
 
     .info {
@@ -91,6 +217,12 @@ let achievement = ref({
         font-size: 14px;
         font-weight: 500;
         color: var(--el-text-color-primary);
+      }
+
+      .level {
+        font-size: 20px;
+        font-weight: 500;
+        color: v-bind(levelColor)
       }
 
       .introduce {
