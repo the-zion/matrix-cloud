89da94ffcd427a8bc00d81f4920a922d2b98013a
liusiyuan <945212191@qq.com>
2022-09-14 18:38:43 +0800

    refactor: update form aside about talk

--- src/pages/talk/component/form.vue
+++ src/pages/talk/component/form.vue
@@ -1,7 +1,11 @@
 <template>
   <el-row class="talk-form">
-    <el-form ref="formRef" v-model="form" label-position="top" class="form">
-      <el-form-item label="标签" class="form-item">
+    <el-form :model="form" class="form" ref="formRef" label-position="top">
+      <el-form-item prop="tags" label="文章标签" class="form-item" :rules="[{
+          required: true,
+          message: '该字段不能为空',
+          trigger: 'blur',
+        }]">
         <el-popover placement="bottom-start" :width="432" trigger="click" :show-arrow="false">
           <template #reference>
             <el-select
@@ -17,13 +21,14 @@
           <matrix-tag v-model:selectedTags="form.tags"></matrix-tag>
         </el-popover>
       </el-form-item>
-      <el-form-item prop="auth" label="发布方式" class="form-item">
+      <el-form-item v-if="mode === 'create'" prop="auth" label="创建方式" class="form-item">
         <el-radio-group v-model="form.auth">
           <el-radio class="radio" v-for="item in authRadio" :label="item.id">{{ item.label }}</el-radio>
         </el-radio-group>
       </el-form-item>
     </el-form>
-    <el-button class="button" type="primary" size="large" @click="createColumn">发布</el-button>
+    <el-button :loading="sending" class="button" type="primary" size="large" @click="commitCheck(formRef)">发布
+    </el-button>
   </el-row>
 </template>
 
@@ -34,47 +39,230 @@ export default {
 </script>
 
 <script setup>
-import {ref} from "vue"
-import {error} from "../../../utils/message";
+import {onBeforeMount, ref} from "vue"
+import {error, warning} from "../../../utils/message";
+import {baseMainStore, userMainStore} from "../../../store";
+import {storeToRefs} from "pinia/dist/pinia.esm-browser";
+import {get, post} from "../../../utils/axios";
+import router from "../../../router";
+import {textEmojiRemove} from "../../../utils/globalFunc";
 
+const userStore = userMainStore()
+const baseStore = baseMainStore()
+const {uuid, cos} = storeToRefs(userStore)
+const {talk} = storeToRefs(baseStore)
+const props = defineProps({
+  mode: String,
+  title: String,
+  id: Number,
+  editor: Object
+})
+
+let title = ref()
+let editor = ref()
+let draftId = ref()
+let mode = ref()
 let form = ref({
-  source: 1,
   auth: 1,
+  text: "",
   tags: [],
 })
 let formRef = ref()
+let sending = ref(false)
+let talkParams = {}
+let introduce = {}
 let authRadio = [{
   id: 1,
   label: "公开"
 }, {
   id: 2,
   label: "私密"
-}, {
-  id: 3,
-  label: "关注者可见"
 }]
-
-
-function checkForm() {
-  if (form.value.tags.length === 0) {
-    error("请选择标签")
-    return false
-  }
-
-  if (form.value.source === 2 && form.value.url === '') {
-    error("请填写转载网址")
-    return false
-  }
-  return true
+let uploadParams = {
+  Bucket: talk.value.bucket,
+  Region: talk.value.region,
 }
 
-function createColumn() {
-  if (!checkForm()) {
-    return null
+let search = {}
+let token = null
+
+function commitCheck(formRef) {
+  if (!formRef) {
+    error("未知错误")
+    return
   }
+
+  formRef.validate((valid, b) => {
+    if (!valid) {
+      error("信息输入有误，请检查")
+    } else {
+      commit()
+      return true
+    }
+  })
 }
 
+function commit() {
+  if (!uuid.value && !token) {
+    warning("账号未登录，请先登录")
+    return
+  }
 
+  if (!draftId.value) {
+    error("讨论发布失败")
+    return
+  }
+
+  if (editor.value.getText().length === 0) {
+    error("内容不能为空白")
+    return
+  }
+
+  setTalkParams()
+  setIntroduceParams()
+  setSearchParams()
+  commitIntroduce()
+}
+
+function setTalkParams() {
+  let image = editor.value.getElemsByType('image')[0]
+  talkParams["title"] = title.value
+  talkParams["html"] = editor.value.getHtml()
+  talkParams["update"] = new Date().toLocaleDateString()
+  talkParams["tags"] = form.value["tags"].join(";")
+  talkParams["auth"] = form.value["auth"]
+  talkParams["id"] = form.value["id"]
+  talkParams["text"] = editor.value.getText().slice(0, 256)
+  talkParams["cover"] = image && image["src"] || ""
+}
+
+function setIntroduceParams() {
+  introduce["id"] = talkParams["id"]
+  introduce["title"] = talkParams["title"]
+  introduce["text"] = talkParams["text"]
+  introduce["update"] = talkParams["update"]
+  introduce["tags"] = talkParams["tags"]
+  introduce["cover"] = talkParams["cover"]
+}
+
+function setSearchParams() {
+  search["uuid"] = uuid.value
+  search["title"] = textEmojiRemove(talkParams["title"])
+  search["text"] = textEmojiRemove(editor.value.getText())
+  search["update"] = talkParams["update"]
+  search["tags"] = talkParams["tags"]
+  search["cover"] = talkParams["cover"]
+}
+
+function commitIntroduce() {
+  sending.value = true
+  uploadParams["Key"] = talk.value.key + uuid.value + "/" + draftId.value + "/introduce" + (mode.value === 'edit' ? "-edit" : "")
+  uploadParams["Headers"] = {
+    'x-cos-meta-token': token,
+  }
+  uploadParams["Body"] = JSON.stringify(introduce)
+  cos.value.uploadFile(uploadParams, function (err) {
+    if (err) {
+      error("讨论发布失败")
+      return
+    }
+    commitSearch()
+  })
+}
+
+function commitSearch() {
+  uploadParams["Key"] = talk.value.key + uuid.value + "/" + draftId.value + "/search"
+  uploadParams["Headers"] = {
+    'x-cos-meta-token': token,
+  }
+  uploadParams["Body"] = JSON.stringify(search)
+  cos.value.uploadFile(uploadParams, function (err) {
+    if (err) {
+      error("讨论发布失败")
+      return
+    }
+    commitTalk()
+  })
+}
+
+function commitTalk() {
+  uploadParams["Key"] = talk.value.key + uuid.value + "/" + draftId.value + "/content" + (mode.value === 'edit' ? "-edit" : "")
+  uploadParams["Headers"] = {
+    'x-cos-meta-token': token,
+    'x-cos-meta-id': draftId.value + "",
+    'x-cos-meta-auth': talkParams.auth,
+    'x-cos-meta-title': encodeURIComponent(title.value),
+    'x-cos-meta-kind': mode.value,
+  }
+  uploadParams["Body"] = JSON.stringify(talkParams)
+  cos.value.uploadFile(uploadParams, function (err) {
+    if (err) {
+      error("讨论发布失败")
+      return
+    }
+    mode.value === 'create' && sendTalk()
+    mode.value === 'edit' && editTalk()
+  })
+}
+
+function sendTalk() {
+  post("/v1/send/talk", {id: draftId.value}).then(function () {
+    router.push({name: "result", query: {type: "success", title: '讨论已提交审核', description: "审核通过即发布到社区中"}})
+  }).catch(function (err) {
+    error("讨论发布失败")
+  }).then(function () {
+    sending.value = false
+  })
+}
+
+function editTalk() {
+  post("/v1/send/talk/edit", {id: draftId.value}).then(function () {
+    router.push({name: "result", query: {type: "success", title: '讨论已提交审核', description: "审核通过即发布到社区中"}})
+  }).catch(function (err) {
+    error("讨论编辑失败")
+  }).then(function () {
+    sending.value = false
+  })
+}
+
+function init() {
+  initData()
+  getData()
+}
+
+function initData() {
+  title.value = props.title
+  editor.value = props.editor
+  draftId.value = props.id
+  mode.value = props.mode
+  token = localStorage.getItem("matrix-token")
+}
+
+function getData() {
+  if (!uuid.value) {
+    return
+  }
+
+  if (!draftId.value) {
+    return
+  }
+
+  let url = talk.value.baseUrl + uuid.value + "/" + draftId.value + "/content"
+  get(url).then(function (reply) {
+    let data = reply.data
+    form.value.id = data["id"]
+    form.value.cover = data["cover"] || ""
+    form.value.auth = data["auth"] || 1
+    form.value.text = data["text"] || ""
+    form.value.tags = data["tags"] ? data["tags"].split(";") : []
+    talkParams["cover"] = data["cover"] || ""
+  }).catch(function () {
+  })
+}
+
+onBeforeMount(function () {
+  init()
+})
 </script>
 
 <style scoped lang="scss">
