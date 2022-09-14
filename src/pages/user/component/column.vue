8346eb959c51b318eda08804f6dbd19f4b7a0091
liusiyuan <945212191@qq.com>
2022-09-14 18:40:12 +0800

    refactor: update page column about user

--- src/pages/user/component/column.vue
+++ src/pages/user/component/column.vue
@@ -2,13 +2,9 @@
   <el-container class="column-container">
     <el-row class="bar" justify="space-between" align="middle">
       <span>专栏</span>
-      <el-space spacer="|" class="operation">
-        <span v-for="item in barMeta" :class="{'select': currentSelect === item.key}" @click="barSelect(item)"
-        >{{ item.label }}</span>
-      </el-space>
     </el-row>
     <el-row class="body">
-      <matrix-column-list></matrix-column-list>
+      <column-user-list ref="listRef"></column-user-list>
     </el-row>
   </el-container>
 </template>
@@ -21,47 +17,48 @@ export default {
 
 <script setup>
 import {ref} from "vue"
+import {userMainStore} from "../../../store";
+import {storeToRefs} from "pinia/dist/pinia";
+import {useRoute} from "vue-router";
+import ColumnUserList from "../../column/component/user.vue";
 
-let barMeta = [{
-  key: "hot",
-  label: "热门"
-}, {
-  key: "new",
-  label: "最新"
-}]
+const userStore = userMainStore()
+const {uuid} = storeToRefs(userStore)
+let visible = ref(false)
+let listRef = ref()
+let userId = useRoute().query["id"]
 
-let currentSelect = ref("hot")
-
-function barSelect(item) {
-  currentSelect.value = item.key
-}
 </script>
 
+
 <style scoped lang="scss">
+
 .column-container {
   width: 100%;
   flex-direction: column;
 
+  ::v-deep(.column-create) {
+    border-radius: 4px;
+    margin-top: 10vh;
+
+    .el-dialog__header {
+      display: none;
+    }
+
+    .el-dialog__body {
+      padding: 20px 24px 0 24px;
+    }
+  }
+
   .bar {
     width: 100%;
     height: 50px;
-    padding: 0 28px;
+    padding: 0 20px;
     border-bottom: 1px solid var(--el-border-color-light);
-
-    .operation {
-      font-size: 14px;
-      color: var(--el-text-color-secondary);
-      cursor: pointer;
-
-      .select {
-        color: var(--el-color-primary);
-      }
-    }
   }
 
   .body {
     width: 100%;
-    margin-bottom: 1rem;
   }
 }
 </style>
