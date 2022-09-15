76967dd94dfa31b6be4a87a1023754cd4b8e8b48
liusiyuan <945212191@qq.com>
2022-09-15 18:24:56 +0800

    refactor: update scroll

--- src/utils/scroll.js
+++ src/utils/scroll.js
@@ -1,3 +1,28 @@
+let delayMethod = null
+
+function handleScroll(fn) {
+    return function () {
+        let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+        let clientHeight = document.documentElement.clientHeight || document.body.clientHeight;
+        let scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
+        let sum = Math.floor(scrollTop + clientHeight)
+        if (sum === scrollHeight) {
+            fn()
+        }
+    }
+}
+
+export function throttle(callback, delay) {
+    let pre = 0
+    return function () {
+        const current = Date.now()
+        if (current - pre > delay) {
+            callback.call(this)
+            pre = current
+        }
+    }
+}
+
 export function scrollToTop() {
     window.scrollTo({
         top: 0
@@ -13,4 +38,13 @@ export function scrollTo(location) {
         top: e.getBoundingClientRect().top + document.documentElement.scrollTop,
         behavior: "smooth"
     })
+}
+
+export function scrollToBottomListen(fn) {
+    delayMethod = throttle(handleScroll(fn), 10)
+    window.addEventListener('scroll', delayMethod);
+}
+
+export function removeScrollToBottomListen() {
+    window.removeEventListener('scroll', delayMethod);
 }
