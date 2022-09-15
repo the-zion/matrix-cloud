06bb0d1bf70160bc3f8f37a9bf669813db896e12
liusiyuan <945212191@qq.com>
2022-09-15 18:24:05 +0800

    refactor: update axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,14 +1,20 @@
 import axios from "axios"
+import {loginTimeOut} from "./globalFunc";
 import {baseMainStore} from "../store";
-import {storeToRefs} from "pinia/dist/pinia.esm-browser";
 
-const baseStore = baseMainStore()
-const {endpoint} = storeToRefs(baseStore)
+let endpoint = null
+
+function getEndPoint() {
+    if (!endpoint) {
+        endpoint = baseMainStore()["endpoint"]
+    }
+    return endpoint
+}
 
 export function post(url, params) {
     let token = localStorage.getItem("matrix-token")
     const instance = axios.create({
-        baseURL: endpoint.value,
+        baseURL: getEndPoint(),
         method: "post",
         headers: {
             'Cache-Control': 'no-cache'
@@ -17,20 +23,63 @@ export function post(url, params) {
     if (token) {
         instance.defaults.headers.common['Authorization'] = "Bearer " + token;
     }
+    token && (instance.defaults.headers.common['Authorization'] = "Bearer " + token)
+    instance.interceptors.response.use(function (response) {
+        return response;
+    }, function (error) {
+        let response = error.response
+        if (response) {
+            switch (response.data.reason) {
+                case "TOKEN_EXPIRED":
+                    loginTimeOut()
+                    break
+            }
+        }
+        return Promise.reject(error)
+    });
     return instance.post(url, params)
 }
 
 export function get(url) {
     let token = localStorage.getItem("matrix-token")
     const instance = axios.create({
-        baseURL: endpoint.value,
+        baseURL: getEndPoint(),
         method: "get",
         headers: {
             'Cache-Control': 'no-cache'
-        }
+        },
     });
     if (token) {
         instance.defaults.headers.common['Authorization'] = "Bearer " + token;
     }
+    instance.interceptors.response.use(function (response) {
+        return response;
+    }, function (error) {
+        let response = error.response
+        if (response) {
+            switch (response.data.reason) {
+                case "TOKEN_EXPIRED":
+                    loginTimeOut()
+                    break
+            }
+        }
+        return Promise.reject(error);
+    });
     return instance.get(url)
+}
+
+export function axiosGetAll(endpoints, resolve, reject, after) {
+    axios.all(endpoints.map((endpoint) => get(endpoint))).then(
+        axios.spread((...allData) => {
+            resolve(allData)
+        })
+    ).catch(reject).then(after);
+}
+
+export function axiosPostAll(endpoints, resolve, reject, after) {
+    axios.all(endpoints.map((endpoint) => post(endpoint))).then(
+        axios.spread((...allData) => {
+            resolve(allData)
+        })
+    ).catch(reject).then(after);
 }
