648b8c371959ea1c28eb29887ff60cd5751105c4
liusiyuan <945212191@qq.com>
2022-09-15 18:22:58 +0800

    refactor: update store

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import {defineStore} from "pinia"
 import {get} from "../utils/axios"
-import {warning} from "../utils/message";
+import {initCos} from "../utils/cos"
 
 export const userMainStore = defineStore("user", {
     state: () => {
@@ -8,12 +8,18 @@ export const userMainStore = defineStore("user", {
             uuid: null,
             username: null,
             avatar: null,
+            cos: null,
         }
     },
 
     getters: {},
 
     actions: {
+        getUserCos(){
+            this.$patch(state => {
+                state.cos = initCos()
+            })
+        },
         getUserProfile() {
             let that = this
             get("/v1/get/user/profile").then(function (reply) {
@@ -22,16 +28,7 @@ export const userMainStore = defineStore("user", {
                     state.username = reply.data.username
                     state.avatar = reply.data.avatar
                 })
-            }).catch(function (err) {
-                let response = err.response
-                if (response) {
-                    switch (response.data.reason) {
-                        case "TOKEN_EXPIRED":
-                            warning("登录已过期，请重新登录")
-                            localStorage.removeItem("matrix-token")
-                            break
-                    }
-                }
+            }).catch(function () {
                 that.$reset()
             })
         }
@@ -42,17 +39,63 @@ export const baseMainStore = defineStore("base", {
     state: () => {
         return {
             endpoint: "http://127.0.0.1:8080",
+            cover: {
+                region: "ap-shanghai",
+                bucket: "user-1309416642",
+                key: "cover/",
+                freezeKey: "audit_freeze_backup/increment_audit/cover/",
+                baseUrl: "https://user-1309416642.cos.ap-shanghai.myqcloud.com/cover/",
+                freezeUrl: "https://user-1309416642.cos.ap-shanghai.myqcloud.com/audit_freeze_backup/increment_audit/cover/"
+            },
             avatar: {
                 region: "ap-shanghai",
                 bucket: "user-1309416642",
                 key: "avatar/",
-                baseUrl: "https://user-1309416642.cos.ap-shanghai.myqcloud.com/avatar/"
+                freezeKey: "audit_freeze_backup/increment_audit/avatar/",
+                baseUrl: "https://user-1309416642.cos.ap-shanghai.myqcloud.com/avatar/",
+                freezeUrl: "https://user-1309416642.cos.ap-shanghai.myqcloud.com/audit_freeze_backup/increment_audit/avatar/"
             },
             article: {
                 region: "ap-shanghai",
                 bucket: "creation-1309416642",
                 key: "article/",
-                baseUrl: "https://creation-1309416642.cos.ap-shanghai.myqcloud.com/article/"
+                freezeKey: "audit_freeze_backup/increment_audit/article/",
+                baseUrl: "https://creation-1309416642.cos.ap-shanghai.myqcloud.com/article/",
+                freezeUrl: "https://creation-1309416642.cos.ap-shanghai.myqcloud.com/audit_freeze_backup/increment_audit/article/"
+            },
+            talk: {
+                region: "ap-shanghai",
+                bucket: "creation-1309416642",
+                key: "talk/",
+                freezeKey: "audit_freeze_backup/increment_audit/talk/",
+                baseUrl: "https://creation-1309416642.cos.ap-shanghai.myqcloud.com/talk/",
+                freezeUrl: "https://creation-1309416642.cos.ap-shanghai.myqcloud.com/audit_freeze_backup/increment_audit/talk/"
+            },
+            column: {
+                region: "ap-shanghai",
+                bucket: "creation-1309416642",
+                key: "column/",
+                freezeKey: "audit_freeze_backup/increment_audit/column/",
+                baseUrl: "https://creation-1309416642.cos.ap-shanghai.myqcloud.com/column/",
+                freezeUrl: "https://creation-1309416642.cos.ap-shanghai.myqcloud.com/audit_freeze_backup/increment_audit/column/"
+            },
+            collect: {
+                region: "ap-shanghai",
+                bucket: "creation-1309416642",
+                key: "collect/",
+                baseUrl: "https://creation-1309416642.cos.ap-shanghai.myqcloud.com/collect/"
+            },
+            collections: {
+                region: "ap-shanghai",
+                bucket: "creation-1309416642",
+                key: "collections/",
+                baseUrl: "https://creation-1309416642.cos.ap-shanghai.myqcloud.com/collections/"
+            },
+            comment: {
+                region: "ap-shanghai",
+                bucket: "comment-1309416642",
+                key: "comment/",
+                baseUrl: "https://comment-1309416642.cos.ap-shanghai.myqcloud.com/comment/"
             },
             images: {
                 baseUrl: "https://images-1309416642.cos.ap-shanghai.myqcloud.com"
