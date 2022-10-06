35a9262671c12336d382fc7e7ad2e0a9a26ed335
liusiyuan <945212191@qq.com>
2022-10-06 18:36:36 +0800

    refactor: add component list about timeline

---
+++ src/pages/timeline/component/list.vue
@@ -0,0 +1,527 @@
+<template>
+  <el-row class="timeline-list" id="timeline-list">
+    <el-empty v-show="data.length === 0 && !loading" class="empty" description=" "
+              :image-size="250" image="../../src/assets/images/no_data.svg"
+    />
+    <el-row class="data" fill :size="0">
+      <el-row v-for="item in data" class="each" :key="item.id"
+              @click="goToPage(item['mode'] === 1?'article':(item['mode'] === 2?'column':'talk'), {id:item.creationId})">
+        <el-row class="article-card" v-if="item['mode'] === 1">
+          <el-space class="main" fill>
+            <el-space class="head">
+              <el-avatar @click.stop="goToPage('user', {id:item.uuid,menu:'timeline'})" class="avatar"
+                         :size="24" icon="UserFilled"
+                         :src="avatar.baseUrl + item.uuid + '/avatar.webp'"/>
+              <el-row class="title">{{ item.title }}</el-row>
+            </el-space>
+            <el-space class="info">
+              <el-row class="time">{{ "发布于 " + item.update }}</el-row>
+              <el-tag round v-show="item.tags" type="info"
+                      v-for="tag in (item.tags?item.tags.split(';'):[])" :key="tag">{{
+                  tag
+                }}
+              </el-tag>
+            </el-space>
+            <el-space class="body" alignment="flex-start">
+              <el-image v-if="item.cover" class="image" fit="cover" :src="item.cover"
+                        lazy></el-image>
+              <span class="content">{{ item.text }}</span>
+            </el-space>
+          </el-space>
+          <el-space class="foot">
+            <el-space :size="3">
+              <el-icon class="iconfont icon-like icon"></el-icon>
+              <span class="num">{{
+                  item.agree > 1000 ? (item.agree / 1000).toFixed(1) + "k" : item.agree || 0
+                }}</span>
+            </el-space>
+            <el-space :size="3">
+              <el-icon class="iconfont icon-eye icon"></el-icon>
+              <span
+                  class="num">{{
+                  item.view > 1000 ? (item.view / 1000).toFixed(1) + "k" : item.view || 0
+                }}</span>
+            </el-space>
+            <el-space :size="3">
+              <el-icon class="iconfont icon-message icon"></el-icon>
+              <span
+                  class="num">{{
+                  item.comment > 1000 ? (item.comment / 1000).toFixed(1) + "k" : item.comment || 0
+                }}</span>
+            </el-space>
+            <el-space :size="3">
+              <el-icon class="iconfont icon-star icon"></el-icon>
+              <span
+                  class="num">{{
+                  item.collect > 1000 ? (item.collect / 1000).toFixed(1) + "k" : item.collect || 0
+                }}</span>
+            </el-space>
+          </el-space>
+        </el-row>
+        <el-row class="column-card" align="top" v-if="item['mode'] === 2">
+          <el-image class="image" fit="cover" :src="item.cover" lazy></el-image>
+          <el-row class="container" align="top">
+            <el-space class="main">
+              <el-row class="title">{{ item.name }}</el-row>
+              <el-space class="info">
+                <el-tag round v-show="item.tags" type="info" v-for="tag in (item.tags?item.tags.split(';'):[])"
+                        :key="tag">{{
+                    tag
+                  }}
+                </el-tag>
+              </el-space>
+            </el-space>
+            <el-row class="content">{{ item.introduce }}</el-row>
+            <el-space class="foot">
+              <el-space :size="3">
+                <el-icon class="iconfont icon-like icon"></el-icon>
+                <span class="num">{{
+                    item.agree > 1000 ? (item.agree / 1000).toFixed(1) + "k" : item.agree
+                  }}</span>
+              </el-space>
+              <el-space :size="3">
+                <el-icon class="iconfont icon-eye icon"></el-icon>
+                <span class="num">{{
+                    item.view > 1000 ? (item.view / 1000).toFixed(1) + "k" : item.view
+                  }}</span>
+              </el-space>
+              <el-space :size="3">
+                <el-icon class="iconfont icon-star icon"></el-icon>
+                <span class="num">{{
+                    item.collect > 1000 ? (item.collect / 1000).toFixed(1) + "k" : item.collect
+                  }}</span>
+              </el-space>
+            </el-space>
+          </el-row>
+        </el-row>
+        <el-row class="talk-card" align="top" v-if="item['mode'] === 3">
+          <el-image v-show="item.cover" class="image" fit="cover" :src="item.cover" lazy></el-image>
+          <el-row class="container" :class="{'full':!item.cover}">
+            <el-space class="main" fill :size="5">
+              <el-space class="head">
+                <el-popover placement="top-start" :show-arrow="false" :width="312" trigger="hover"
+                            popper-class="popover" @before-enter="item['showUserCard'] = true"
+                            @after-leave="item['showUserCard'] = false">
+                  <template #reference>
+                    <el-avatar @click.stop="goToPage('user', {id:item.uuid,menu:'timeline'})" class="avatar"
+                               :size="24" icon="UserFilled"
+                               :src="avatar.baseUrl + item.uuid + '/avatar.webp'"/>
+                  </template>
+                  <matrix-user-mini-card :uuid="item.uuid" v-if="item['showUserCard']"></matrix-user-mini-card>
+                </el-popover>
+                <el-row class="title">{{ item.title }}</el-row>
+              </el-space>
+              <el-space class="info">
+                <el-row class="time">{{ "发布于 " + item.update }}</el-row>
+                <el-tag round v-show="item.tags" type="info" v-for="tag in (item.tags?item.tags.split(';'):[])"
+                        :key="tag">{{
+                    tag
+                  }}
+                </el-tag>
+              </el-space>
+              <span class="content">{{ item.text }}</span>
+            </el-space>
+            <el-space class="foot">
+              <el-space :size="3">
+                <el-icon class="iconfont icon-like icon"></el-icon>
+                <span class="num">{{ item.agree > 1000 ? (item.agree / 1000).toFixed(1) + "k" : item.agree }}</span>
+              </el-space>
+              <el-space :size="3">
+                <el-icon class="iconfont icon-eye icon"></el-icon>
+                <span class="num">{{ item.view > 1000 ? (item.view / 1000).toFixed(1) + "k" : item.view }}</span>
+              </el-space>
+              <el-space :size="3">
+                <el-icon class="iconfont icon-message icon"></el-icon>
+                <span class="num">{{
+                    item.comment > 1000 ? (item.comment / 1000).toFixed(1) + "k" : item.comment
+                  }}</span>
+              </el-space>
+              <el-space :size="3">
+                <el-icon class="iconfont icon-star icon"></el-icon>
+                <span class="num">{{
+                    item.collect > 1000 ? (item.collect / 1000).toFixed(1) + "k" : item.collect
+                  }}</span>
+              </el-space>
+            </el-space>
+          </el-row>
+        </el-row>
+      </el-row>
+    </el-row>
+    <el-skeleton class="skeleton" v-show="loading" :rows="3" animated/>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: "TimeLineList"
+}
+</script>
+
+<script setup>
+import {ref, onBeforeMount} from "vue";
+import {throttle, scrollToBottomListen} from "../../../utils/scroll";
+import {goToPage} from "../../../utils/globalFunc";
+import {info} from "../../../utils/message";
+import {axiosGetAll, get} from "../../../utils/axios";
+import {baseMainStore} from "../../../store";
+import {storeToRefs} from "pinia/dist/pinia.esm-browser";
+import {useRoute} from "vue-router";
+
+const baseStore = baseMainStore()
+const {avatar, article, column, talk} = storeToRefs(baseStore)
+
+let data = ref([])
+let list = ref([])
+let currentPage = 1
+let loading = ref(false)
+let userId = ref()
+let isBottom = false
+let mode = "new"
+let getDataLock = false
+
+function init() {
+  initData()
+  getData()
+}
+
+function initData() {
+  userId.value = useRoute().query["id"]
+  scrollToBottomListen(throttle(scrollToBottom, 1000))
+}
+
+function scrollToBottom() {
+  getData()
+}
+
+function getData() {
+  if (isBottom || getDataLock || !userId.value) {
+    return
+  }
+
+  loading.value = true
+  getDataLock = true
+  get("/v1/get/user/timeline/list/visitor?page=" + currentPage + "&uuid=" + userId.value).then(function (reply) {
+    list.value = reply.data.timeline
+    let size = reply.data.timeline.length
+    if (size === 0) {
+      isBottom = true
+      loading.value = false
+      getDataLock = false
+      return
+    }
+    currentPage += 1
+    getIntroduce()
+  }).catch(function () {
+    loading.value = false
+    getDataLock = false
+  })
+}
+
+function getIntroduce() {
+  let endpoints = []
+  list.value.forEach(function (item) {
+    endpoints.push((item["mode"] === 1 ? article.value.baseUrl : (item["mode"] === 2 ? column.value.baseUrl : talk.value.baseUrl)) + item["uuid"] + "/" + item["creationId"] + "/introduce")
+  })
+  axiosGetAll(endpoints, function (allData) {
+    allData.forEach(function (each) {
+      list.value.forEach(function (item, index) {
+        each.data.id === item["creationId"] && (list.value[index] = Object.assign(item, each.data))
+      })
+    })
+  }, function () {
+  }, function () {
+    data.value = data.value.concat(list.value)
+    loading.value = false
+    getDataLock = false
+  })
+}
+
+function userChange(m) {
+  isBottom = false
+  currentPage = 1
+  data.value = []
+  userId.value = m
+  getData()
+}
+
+defineExpose({
+  userChange
+})
+
+onBeforeMount(() => {
+  init()
+})
+
+</script>
+
+<style scoped lang="scss">
+.timeline-list {
+  width: 100%;
+
+  .empty {
+    width: 100%;
+  }
+
+  .data {
+    width: 100%;
+
+    .each {
+      position: relative;
+      border-bottom: 1px solid var(--el-border-color-lighter);
+      background-color: var(--el-color-white);
+      cursor: pointer;
+      width: 100%;
+
+      .article-card {
+        width: 100%;
+        padding: 16px;
+
+        .main {
+          width: 100%;
+
+          .head {
+            width: 100%;
+
+            ::v-deep(.el-avatar--circle) {
+              border: 1px solid var(--el-border-color-lighter);
+            }
+
+            .avatar {
+              font-size: 14px;
+              border: 1px solid var(--el-border-color-lighter);
+            }
+
+            .title {
+              font-size: 16px;
+              font-weight: 500;
+              color: var(--el-text-color-primary)
+            }
+
+            .title:hover {
+              color: var(--el-color-primary);
+            }
+          }
+
+          .info {
+            width: 100%;
+
+            .name {
+              font-size: 14px;
+              color: var(--el-text-color-secondary)
+            }
+
+            .time {
+              font-size: 14px;
+              color: var(--el-text-color-regular)
+            }
+          }
+
+          .body {
+            width: 100%;
+            max-height: 80px;
+
+            .image {
+              height: 80px;
+              width: 120px;
+              border-radius: 6px;
+            }
+
+            .content {
+              font-size: 14px;
+              line-height: 24px;
+              color: var(--el-text-color-regular);
+              word-break: break-word;
+              align-self: stretch;
+              overflow: hidden;
+              display: -webkit-box;
+              -webkit-box-orient: vertical;
+              -webkit-line-clamp: 3;
+            }
+          }
+        }
+
+        .foot {
+          width: 100%;
+          margin-top: 1rem;
+
+          .icon {
+            font-size: 18px;
+            color: var(--el-text-color-secondary)
+          }
+
+          .num {
+            font-size: 14px;
+            color: var(--el-text-color-secondary)
+          }
+        }
+      }
+
+      .column-card {
+        width: 100%;
+        padding: 16px;
+
+        .image {
+          height: 120px;
+          width: 170px;
+          border-radius: 6px;
+          margin-right: 16px;
+        }
+
+        .container {
+          width: 70%;
+          height: 100%;
+        }
+
+        .main {
+          width: 100%;
+          height: fit-content;
+
+          .title {
+            font-size: 16px;
+            line-height: 24px;
+            font-weight: 500;
+            color: var(--el-text-color-secondary)
+          }
+
+          .title:hover {
+            color: var(--el-color-primary);
+          }
+
+          .info {
+            width: 100%;
+          }
+        }
+
+        .content {
+          margin-top: 8px;
+          height: 40px;
+          font-size: 14px;
+          line-height: 20px;
+          color: var(--el-text-color-secondary);
+          word-break: break-word;
+          align-self: stretch;
+          overflow: hidden;
+          display: -webkit-box;
+          -webkit-box-orient: vertical;
+          -webkit-line-clamp: 2;
+        }
+
+        .foot {
+          width: 100%;
+          margin-top: 8px;
+
+          .icon {
+            font-size: 18px;
+            color: var(--el-text-color-secondary)
+          }
+
+          .num {
+            font-size: 14px;
+            color: var(--el-text-color-secondary)
+          }
+        }
+      }
+
+      .talk-card {
+        width: 100%;
+        padding: 16px;
+
+        .image {
+          height: 125px;
+          width: 180px;
+          border-radius: 6px;
+          margin-right: 16px;
+
+        }
+
+        .container {
+          width: calc(100% - 216px);
+
+          .main {
+            width: 100%;
+
+            .head {
+              width: 100%;
+
+              ::v-deep(.el-avatar--circle) {
+                border: 1px solid var(--el-border-color-lighter);
+              }
+
+              .avatar {
+                font-size: 14px;
+                border: 1px solid var(--el-border-color-lighter);
+              }
+
+              .title {
+                font-size: 16px;
+                font-weight: 500;
+                color: var(--el-text-color-primary)
+              }
+
+              .title:hover {
+                color: var(--el-color-primary);
+              }
+
+            }
+
+            .info {
+              width: 100%;
+
+              .time {
+                font-size: 14px;
+                color: var(--el-text-color-regular)
+              }
+            }
+
+            .content {
+              width: 100%;
+              height: 43px;
+              font-size: 14px;
+              line-height: 20px;
+              color: var(--el-text-color-regular);
+              word-break: break-word;
+              align-self: stretch;
+              overflow: hidden;
+              display: -webkit-box;
+              -webkit-box-orient: vertical;
+              -webkit-line-clamp: 2;
+            }
+          }
+
+          .foot {
+            width: 100%;
+            margin-top: 5px;
+
+            .icon {
+              font-size: 18px;
+              color: var(--el-text-color-secondary)
+            }
+
+            .num {
+              font-size: 14px;
+              color: var(--el-text-color-secondary)
+            }
+          }
+        }
+
+        .full {
+          width: 100%;
+        }
+      }
+    }
+
+    .each:last-child {
+      border-bottom: unset;
+    }
+  }
+
+  .skeleton {
+    padding: 16px;
+  }
+
+  .list-foot {
+    margin: 1rem;
+    width: 100%;
+  }
+}
+</style>
