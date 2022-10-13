b1fb3fe27443a42de0a21534219aaebba42d5aa4
liusiyuan <945212191@qq.com>
2022-10-13 18:26:19 +0800

    refactor: update avatar size

--- src/pages/article/component/user.vue
+++ src/pages/article/component/user.vue
@@ -9,7 +9,7 @@
         <el-row class="article-card">
           <el-space class="main" fill>
             <el-space class="head">
-              <el-avatar class="avatar" :size="24" icon="UserFilled"
+              <el-avatar class="avatar" :size="32" icon="UserFilled"
                          :src="avatar.baseUrl + item.uuid + '/avatar.webp'"/>
               <el-row class="title">{{ item.title }}</el-row>
             </el-space>
@@ -191,7 +191,7 @@ onBeforeMount(function () {
             }
 
             .avatar {
-              font-size: 14px;
+              font-size: 20px;
               border: 1px solid var(--el-border-color-lighter);
             }
 
