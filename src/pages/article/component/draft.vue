05d355f467987c46067ccd9e10a398c735eba4ca
liusiyuan <945212191@qq.com>
2022-12-21 22:12:23 +0800

    feat: add draft delete

--- src/pages/article/component/draft.vue
+++ src/pages/article/component/draft.vue
@@ -3,10 +3,13 @@
     <el-skeleton class="skeleton" v-show="loading" :rows="1" animated/>
     <el-space v-show="!loading" v-for="item in draft" :key="item.id" class="each" :size="15" @click="draftSelect(item)">
       <el-image :src="item.cover" v-show="item.cover" class="image" fit="cover"></el-image>
-      <el-space fill>
+      <el-space fill class="block">
         <span class="title">{{ item.title || "暂无标题" }}</span>
         <span class="time">{{ item.update ? ("最近编辑于 " + item.update) : "暂未编辑" }}</span>
       </el-space>
+      <el-icon class="delete" @click.stop="draftDelete(item.id)">
+        <Delete/>
+      </el-icon>
     </el-space>
     <el-empty v-show="!loading && draft.length === 0" class="empty" description=" "
               :image-size="150" :image="noData"
@@ -27,7 +30,8 @@ import {userMainStore} from "../../../store/user";
 import {baseMainStore} from "../../../store/base";
 import {storeToRefs} from "pinia/dist/pinia.esm-browser";
 import {warning} from "../../../utils/message";
-import {getAssets} from "../../../utils/globalFunc";
+import {confirm, getAssets} from "../../../utils/globalFunc";
+import {error} from "../../../utils/message";
 
 const userStore = userMainStore()
 const baseStore = baseMainStore()
@@ -50,11 +54,11 @@ function getData() {
   loading.value = true
   get("/v1/get/article/draft/list").then(function (reply) {
     draft.value = reply.data["draft"]
-    if(!draft.value.length){
+    if (!draft.value.length) {
       loading.value = false
     }
     getFromCos()
-  }).catch(function (){
+  }).catch(function () {
     loading.value = false
   })
 }
@@ -80,6 +84,20 @@ function getFromCos() {
 
 function draftSelect(item) {
   emits("draftSelect", item.id)
+}
+
+function draftDelete(id) {
+  confirm("稿件删除", "确认删除该稿件吗").then(function () {
+    loading.value = true
+    post("/v1/delete/article/draft", {
+      id: id,
+    }).then(function () {
+      getData()
+    }).catch(function () {
+      error("稿件删除失败")
+      loading.value = false
+    })
+  })
 }
 
 onBeforeMount(function () {
@@ -112,14 +130,27 @@ onBeforeMount(function () {
       border-radius: 3px;
     }
 
-    .title {
-      font-size: 14px;
-      line-height: 22px;
-      color: var(--el-text-color-primary);
+    .block {
+      word-break: break-all;
+
+      .title {
+        font-size: 14px;
+        line-height: 22px;
+        color: var(--el-text-color-primary);
+      }
+
+      .time {
+        font-size: 12px;
+        line-height: 16px;
+        color: var(--el-text-color-placeholder);
+      }
     }
 
-    .time {
-      font-size: 12px;
+    .delete {
+      position: absolute;
+      right: 20px;
+      bottom: 10px;
+      font-size: 15px;
       line-height: 16px;
       color: var(--el-text-color-placeholder);
     }
